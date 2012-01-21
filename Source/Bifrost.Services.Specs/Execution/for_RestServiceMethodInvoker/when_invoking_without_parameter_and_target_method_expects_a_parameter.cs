e648215284ded711037682281fbe78ba9bb20611
Einar Ingebrigtsen <einar@dolittle.com>
2012-01-21 15:37:58 +0100

    Removing the specs from the Blog sample - the sample itself has not been maintained for a while.

---
+++ Source/Bifrost.Services.Specs/Execution/for_RestServiceMethodInvoker/when_invoking_without_parameter_and_target_method_expects_a_parameter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bifrost.Services.Specs.Execution.for_RestServiceMethodInvoker
+{
+    public class when_invoking_without_parameter_and_target_method_expects_a_parameter : given.a_rest_service_method_invoker
+    {
+    }
+}
