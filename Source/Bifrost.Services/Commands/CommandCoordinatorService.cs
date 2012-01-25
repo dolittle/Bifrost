f4c7432c7fe1d75ff682eb99fe4e876bc8a2f115
Einar Ingebrigtsen <einar@dolittle.com>
2012-01-25 11:38:11 +0100

    Adding support for performing multiple commands in one go against a Saga - doing it hardcore on the CommandCoordinator Service to not rock the boat in the underlying implementation for now. This is probably something we want to push downwards as a "first class citizen" behavior in the platform.

--- Source/Bifrost.Services/Commands/CommandCoordinatorService.cs
+++ Source/Bifrost.Services/Commands/CommandCoordinatorService.cs
@@ -24,6 +24,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Bifrost.Commands;
 using Bifrost.Execution;
+using Bifrost.Sagas;
 using Bifrost.Serialization;
 
 namespace Bifrost.Services.Commands
@@ -33,37 +34,64 @@ namespace Bifrost.Services.Commands
         ICommandCoordinator _commandCoordinator;
         ISerializer _serializer;
         ITypeDiscoverer _typeDiscoverer;
+        ISagaLibrarian _sagaLibrarian;
         Dictionary<string, Type> _commandTypes;
+
 
         public CommandCoordinatorService(
             ICommandCoordinator commandCoordinator, 
             ISerializer serializer,
-            ITypeDiscoverer typeDiscoverer)
+            ITypeDiscoverer typeDiscoverer,
+            ISagaLibrarian sagaLibrarian)
         {
             _commandCoordinator = commandCoordinator;
             _serializer = serializer;
             _typeDiscoverer = typeDiscoverer;
+            _sagaLibrarian = sagaLibrarian;
             PopulateCommandTypes();
         }
 
+        public CommandResult Handle(CommandDescriptor commandDescriptor)
+        {
+            var commandInstance = GetCommandFromDescriptor(commandDescriptor);
+            if (commandInstance == null)
+                return new CommandResult { Exception = new UnknownCommandException(commandDescriptor.Name) };
+
+            var result = _commandCoordinator.Handle(commandInstance);
+            return result;
+        }
+
+        public IEnumerable<CommandResult> HandleForSaga(Guid sagaId, IEnumerable<CommandDescriptor> commandDescriptors)
+        {
+            var results = new List<CommandResult>();
+            var saga = _sagaLibrarian.Get(sagaId);
+
+            foreach (var commandDescriptor in commandDescriptors)
+            {
+                var commandInstance = GetCommandFromDescriptor(commandDescriptor);
+                if (commandInstance == null)
+                    return new[] { new CommandResult { Exception = new UnknownCommandException(commandDescriptor.Name) }};
+
+                results.Add(_commandCoordinator.Handle(saga, commandInstance));
+            }
+
+            return results.ToArray();
+        }
 
         void PopulateCommandTypes()
         {
             var commands = _typeDiscoverer.FindMultiple<ICommand>();
-            _commandTypes = commands.Select(c=>c).ToDictionary(c => c.Name);
+            _commandTypes = commands.Select(c => c).ToDictionary(c => c.Name);
         }
 
-
-        public CommandResult Handle(CommandDescriptor commandDescriptor)
+        ICommand GetCommandFromDescriptor(CommandDescriptor commandDescriptor)
         {
             var commandName = commandDescriptor.Name;
             if (_commandTypes.ContainsKey(commandName))
             {
                 var commandInstance = _serializer.FromJson(_commandTypes[commandName], commandDescriptor.Command) as ICommand;
-                var result = _commandCoordinator.Handle(commandInstance);
-                return result;
+                return commandInstance;
             }
-            
             return null;
         }
     }
