1e3446d74f6166cd1218ba3f4ebc207532e108fe
Einar Ingebrigtsen <einar@dolittle.com>
2012-01-31 08:20:58 +0100

    Adding forseti to the mix and cleaning up the Build file to not show S3 passwords in cleartext. :)

---
+++ Source/Bifrost.JavaScript/Bifrost.js
@@ -0,0 +1,9 @@
+/*
+	@depends utils.js
+	@depends Commands/Command.js
+    @depends Commands/CommandDescriptor.js
+    @depends Commands/commandCoordinator.js
+    @depends Sagas/Saga.js
+    @depends Sagas/sagaNarrator.js
+	@depends features.js
+*/
