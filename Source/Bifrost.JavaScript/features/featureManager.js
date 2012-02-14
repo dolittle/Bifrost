e315387ee515366267affee59bcb6dc3ee9dde46
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-14 22:01:09 +0100

    Sub features are now supported - meaning, features within features!  Kinda neat!

--- Source/Bifrost.JavaScript/features/featureManager.js
+++ Source/Bifrost.JavaScript/features/featureManager.js
@@ -16,6 +16,14 @@ Bifrost.features.featureManager = (function () {
             allFeatures[name] = feature;
             return feature;
         },
+        hookup: function ($) {
+            $("*[data-feature]").each(function () {
+                var target = $(this);
+                var name = $(this).attr("data-feature");
+                var feature = Bifrost.features.featureManager.get(name);
+                feature.renderTo(target[0]);
+            });
+        },
         all: function () {
             return allFeatures;
         }
--- Source/Bifrost.JavaScript/features/loader.js
+++ Source/Bifrost.JavaScript/features/loader.js
@@ -1,11 +1,6 @@
 (function ($) {
     $(function () {
-        $("*[data-feature]").each(function () {
-            var target = $(this);
-            var name = $(this).attr("data-feature");
-            var feature = Bifrost.features.featureManager.get(name);
-            feature.renderTo(target[0]);
-        });
+        Bifrost.features.featureManager.hookup($);
     });
 })(jQuery);
 
