5c1ed891faa5dba7549baeb1ab9d86e20d32911f
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-20 22:51:17 +0100

    Started fleshing out Validation MetaData

---
+++ Source/Bifrost.Specs/Commands/for_CommandTypeManager/when_creating_an_instance_of_command_type_manager_and_multiple_commands_with_same_name_exists.cs
@@ -0,0 +1,17 @@
+using Bifrost;
+
+namespace Bifrost.Specs.Commands.for_CommandTypeManager
+{
+	public class when_creating_an_instance_of_command_type_manager_and_multiple_commands_with_same_name_exists : given.a_command_type_manager
+	{
+		static Exception exception;
+		
+		
+		Establish context = () => type_discoverer_mock.Setup(t=>t.FindMultiple<ICommand>()).Returns(new[] {typeof(SomeCommand), typeof(SomeCommand)});
+		
+		Because of = () => exception = Catch.Exception(()=>command_type_manager = new CommandTypeManager(type_discoverer_mock.Object));
+		
+		
+	}
+}
+
