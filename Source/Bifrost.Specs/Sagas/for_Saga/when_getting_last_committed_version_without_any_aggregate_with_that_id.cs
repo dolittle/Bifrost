dd9108bede59b9dc042e9a5566d775f73c0b7271
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-28 11:29:05 +0100

    Added an implementation for getting the highest committed version number of a specific aggregate inside a Saga - it threw NotImplementedException earlier!

---
+++ Source/Bifrost.Specs/Sagas/for_Saga/when_getting_last_committed_version_without_any_aggregate_with_that_id.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Bifrost.Events;
+using Bifrost.Fakes.Commands;
+using Machine.Specifications;
+
+namespace Bifrost.Specs.Sagas.for_Saga
+{
+    public class when_getting_last_committed_version_without_any_aggregate_with_that_id : given.a_saga
+    {
+        static EventSourceVersion version;
+ 
+        Because of = () => version = saga.GetLastCommittedVersion(typeof(SimpleCommand), Guid.NewGuid());
+
+        It should_return_a_zero_version = () => version.ShouldEqual(EventSourceVersion.Zero);
+    }
+}
