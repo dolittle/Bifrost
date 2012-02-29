c7eb59abb7ae3babef591805f6171700fe0b5684
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-29 14:16:13 +0100

    Adding Id as part of the Command content - so it will be serialized and sent to the server as well.

--- Source/Bifrost.JavaScript/Bifrost.debug.js
+++ Source/Bifrost.JavaScript/Bifrost.debug.js
@@ -558,6 +558,12 @@ Bifrost.namespace("Bifrost.commands");
 Bifrost.commands.CommandDescriptor = (function () {
     function CommandDescriptor(name, id, commandParameters) {
         this.Name = name;
+		var commandContent = {
+			Id: id
+		};
+		for( var property in commandParameters ) {
+			commandContent[property] = commandParameters[property];
+		}
         this.Command = ko.toJSON(commandParameters);
     };
 
