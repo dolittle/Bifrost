dea88e85515431226f8ba94c2f9a8b1f58677254
Einar Ingebrigtsen <einar@dolittle.com>
2012-02-29 14:09:01 +0100

    Changing to Knockouts version for converting to JSON, it is observable aware and gets the value in case of that

--- Source/Bifrost.JavaScript/Commands/CommandDescriptor.js
+++ Source/Bifrost.JavaScript/Commands/CommandDescriptor.js
@@ -2,20 +2,7 @@ Bifrost.namespace("Bifrost.commands");
 Bifrost.commands.CommandDescriptor = (function () {
     function CommandDescriptor(name, id, commandParameters) {
         this.Name = name;
-
-        var commandContent = {
-            Id: id
-        };
-
-        for (var parameter in commandParameters) {
-            if (typeof (commandParameters[parameter]) == "function") {
-                commandContent[parameter] = commandParameters[parameter]();
-            } else {
-                commandContent[parameter] = commandParameters[parameter];
-            }
-        }
-
-        this.Command = JSON.stringify(commandContent);
+        this.Command = ko.toJSON(commandParameters);
     };
 
     return {
