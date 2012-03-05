9ff643a7c5fda26e7375fd5295838d3e80cadb9b
Einar Ingebrigtsen <einar@dolittle.com>
2012-03-05 10:15:12 +0100

    Started fleshing out a state for Features - this state can then be part of history - maybe we should have Transient state as part of it and Permanent state, the difference between the two being Permanent would be on the URL and the Transient one be part of the application while running..

---
+++ Source/Bifrost.JavaScript/features/FeatureState.js
@@ -0,0 +1,13 @@
+Bifrost.namespace("Bifrost.features");
+Bifrost.features.FeatureState = (function () {
+	function FeatureState() {
+		
+	}
+	
+	return {
+		create: function() {
+			var featureState = new FeatureState();
+			return featureState;
+		}
+	}
+})();
