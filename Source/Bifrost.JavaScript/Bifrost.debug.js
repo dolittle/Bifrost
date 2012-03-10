300073ee4d46113fbcb49414be5d099830eada3b
Einar Ingebrigtsen <einar@dolittle.com>
2012-03-10 07:37:51 +0100

    Changing the meaning of Bifrost.Web - it was a small test project, but now it is in fact Web without MVC and the MVC extension depends on it. Now we can create SPAs without having to have an MVC dependency

--- Source/Bifrost.JavaScript/Bifrost.debug.js
+++ Source/Bifrost.JavaScript/Bifrost.debug.js
@@ -846,31 +846,39 @@ Bifrost.features.uriMapper = (function () {
 })();
 Bifrost.namespace("Bifrost.features");
 Bifrost.features.ViewModel = (function () {
-    function ViewModel(definition, isSingleton, state) {
+    function ViewModel(definition, options) {
         var self = this;
         this.definition = definition;
-        this.isSingleton = isSingleton;
-		this.state = {};
-		
-		Bifrost.extend(this.state, state);
+        this.options = {
+            isSingleton: false
+        }
+        Bifrost.extend(this.options, options);
+
+        if (options && options.state) {
+            this.state = Bifrost.features.FeatureState.create(options.state);
+        } else {
+            this.state = {};
+        }
 
         this.getInstance = function () {
-
-            if (self.isSingleton) {
+            if (self.options.isSingleton) {
                 if (!self.instance) {
                     self.instance = new self.definition();
+                    self.instance.state = self.state;
                 }
 
                 return self.instance;
             }
 
-            return new self.definition();
+            var instance = new self.definition();
+            instance.state = self.state;
+            return instance;
         };
     }
 
     return {
-        create: function (definition, isSingleton, state) {
-            var viewModel = new ViewModel(definition, isSingleton, state);
+        create: function (definition, options) {
+            var viewModel = new ViewModel(definition, options);
             return viewModel;
         }
     }
@@ -910,8 +918,8 @@ Bifrost.features.Feature = (function () {
             });
         }
 
-        this.defineViewModel = function (viewModel, isSingleton, state) {
-            self.viewModel = Bifrost.features.ViewModel.create(viewModel, isSingleton, state);
+        this.defineViewModel = function (viewModel, options) {
+            self.viewModel = Bifrost.features.ViewModel.create(viewModel, options);
         }
 
         this.renderTo = function (target) {
--- Source/Bifrost.Web/Properties/AssemblyInfo.cs
+++ Source/Bifrost.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bifrost.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b7600cb-0713-4512-8513-e755acdbb7fd")]
+[assembly: Guid("42275d39-a4f1-4445-91d0-c6d45f7db356")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
