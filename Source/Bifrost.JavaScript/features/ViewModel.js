a00105f52fc0153bee6b26fe7538aaa1f2634f53
Einar Ingebrigtsen <einari@Einar-Ingebrigtsens-iMac.local>
2012-04-06 13:57:12 +0200

    New version of Forseti

--- Source/Bifrost.JavaScript/features/ViewModel.js
+++ Source/Bifrost.JavaScript/features/ViewModel.js
@@ -1,10 +1,6 @@
 Bifrost.namespace("Bifrost.features");
 Bifrost.features.ViewModel = (function() {
 	function ViewModel() {
-		
-		this.coolShit = function() {
-			print("Cool as hell");
-		}
 	}
 	
 	return {
