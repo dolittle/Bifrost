04fe28a971da2cc1e7f060868806b62f1e611cc7
Einar Ingebrigtsen <einar@dolittle.com>
2012-04-12 14:44:17 +0200

    Registering the DefaultTypeLoader with the container at creation

--- Source/Bifrost.Windsor/Container.cs
+++ Source/Bifrost.Windsor/Container.cs
@@ -14,6 +14,7 @@ namespace Bifrost.Windsor
 		public Container(IWindsorContainer windsorContainer)
 		{
 			_windsorContainer = windsorContainer;
+			_windsorContainer.Register (Component.For<DefaultTypeLoader>());
 		}
 		
 		
