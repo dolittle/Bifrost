deb63bfc590b2a6271a0b457ef5ce609b3b3262f
Einar Ingebrigtsen <einar@dolittle.com>
2012-05-15 19:00:27 +0200

    Started making Namespace better and started work on Class

---
+++ Source/Bifrost.JavaScript.Specs/Utils/for_namespace/when_declaring_namespace_with_content.js
@@ -0,0 +1,17 @@
+describe("when declaring namespace with content", function() {
+	Bifrost.namespace("Namespace.With.Content", {
+		a_function: function() {
+			
+		},
+		
+		a_value: 42
+	});
+	
+	it("should include function from content", function() {
+		expect(Namespace.With.Content.a_function).toBeDefined();
+	});
+	
+	it("should include value from content", function() {
+		expect(Namespace.With.Content.a_value).toBe(42);
+	});
+});
