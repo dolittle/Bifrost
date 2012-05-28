e883489e10b38b74136c0cffd0b7283a280a1a73
Pavneet Singh Saund <pav.saund@gmail.com>
2012-05-28 22:16:08 +0200

    Added specs for thought usage
    Specs showing simple usage

--- Source/Bifrost.Fakes/Sagas/TransitionalChapterTransitionValidator.cs
+++ Source/Bifrost.Fakes/Sagas/TransitionalChapterTransitionValidator.cs
@@ -1,9 +1,15 @@
 using Bifrost.Validation;
+using FluentValidation;
 
 namespace Bifrost.Fakes.Sagas
 {
     public class TransitionalChapterTransitionValidator : ChapterTranstionValidator<TransitionalChapter, SimpleChapter>
     {
+        public TransitionalChapterTransitionValidator()
+        {
+            RuleFor(c => c.Something).NotEmpty();
+            RuleFor(c => c.AnInteger).GreaterThanOrEqualTo(1);
+        }
 
     }
 }
