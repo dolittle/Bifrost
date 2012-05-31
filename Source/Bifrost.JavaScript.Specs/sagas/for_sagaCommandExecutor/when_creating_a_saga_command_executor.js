60046eb20c1acc793e9fb416d398ecc9980d371f
Marius Gundersen <gundersen@gmail.com>
2012-05-31 17:37:14 +0200

    started working on the Sagas/sagaCommandExecutor.js

---
+++ Source/Bifrost.JavaScript.Specs/sagas/for_sagaCommandExecutor/when_creating_a_saga_command_executor.js
@@ -0,0 +1,50 @@
+﻿describe("when creating a saga command executor", function () {
+    var sce,
+        options;
+
+    beforeEach(function () {
+        options = {
+            sagaId: "bleh",
+            commands: [1, 2, 3],
+            beforeExecute: function (){ },
+            success: function () { },
+            complete: function () { },
+            error: function () { }
+            
+        };
+        sce = Bifrost.sagas.SagaCommandExecutor.create(options);
+
+    });
+
+    it("should expose an execute method", function () {
+        expect(sce.execute).toBeDefined();
+    });
+
+    it("should expose if it is busy", function () {
+        expect(sce.isBusy).toBeDefined();
+    });
+
+    it("should use the sagaId from the options", function () {
+        expect(sce.sagaId).toBe(options.sagaId);
+    });
+
+    it("should use the commands from the options", function () {
+        expect(sce.commands).toBe(options.commands);
+    });
+
+    it("should use the success callback from the options", function () {
+        expect(sce.options.success).toBe(options.success);
+    });
+
+    it("should use the beforeExecute callback from the options", function () {
+        expect(sce.options.beforeExecute).toBe(options.beforeExecute);
+    });
+
+    it("should use the error callback from the options", function () {
+        expect(sce.options.error).toBe(options.error);
+    });
+
+    it("should use the complete callback from the options", function () {
+        expect(sce.options.complete).toBe(options.complete);
+    });
+});
---
+++ Source/Bifrost.JavaScript/Sagas/sagaCommandExecutor.js
@@ -0,0 +1,97 @@
+﻿Bifrost.namespace("Bifrost.sagas");
+Bifrost.sagas.SagaCommandExecutor = (function () {
+    function SagaCommandExecutor(options) {
+        var self = this;
+
+        this.isBusy = ko.observable(false);
+        this.sagaId = options.sagaId || Bifrost.Guid.empty;
+        this.commands = options.commands || [];
+        
+        this.options = {
+            beforeExecute: function () {
+            },
+            error: function () {
+            },
+            success: function () {
+            },
+            complete: function () {
+            }
+        };
+        // Todo: add an overrideWith or similar that will always pick the one that is not undefined!
+        // add validation check for "type" based on source, if not function for instance in the merger
+        // exception!
+        Bifrost.extend(this.options, options);
+
+
+        this.execute = function () {
+
+
+
+            if (self.onBeforeExecute() === false) {
+                return;
+            }
+
+            Bifrost.commands.commandCoordinator.handleForSaga(self, self.commands, {
+                error: function (e) {
+                    self.onError(e);
+                },
+                complete: function () {
+                    self.onComplete();
+                },
+                success: function () {
+                    self.onSuccess();
+                }
+            });
+        };
+
+        this.onBeforeExecute = function () {
+
+            self.hasError = false;
+
+            self.validate();
+            if (!self.parametersAreValid()) {
+                return false;
+            }
+
+            self.options.beforeExecute.call(self.viewModel, self);
+
+            if (!self.canExecute.call(self.viewModel)) {
+                return false;
+            }
+            self.isBusy(true);
+            self.id = Bifrost.Guid.create();
+
+            return true;
+        };
+
+        this.onError = function () {
+            self.hasError = true;
+            if (self.result.hasOwnProperty("validationResults")) {
+                self.applyServerValidation(self.result.validationResults);
+            }
+            self.options.error.call(self.viewModel, self.result);
+        };
+
+        this.onSuccess = function () {
+            self.hasError = false;
+            self.resetAllValidationMessages();
+            self.options.success.call(self.viewModel, self.result);
+        };
+
+        this.onComplete = function () {
+            if (!self.hasError) {
+                self.options.complete.call(self.viewModel, self.result);
+            }
+            self.isBusy(false);
+        };
+
+    }
+
+    return {
+        create: function (options) {
+            var sagaCommandExecutor = new SagaCommandExecutor(options);
+            Bifrost.extend(sagaCommandExecutor, options);
+            return sagaCommandExecutor;
+        }
+    };
+})();
