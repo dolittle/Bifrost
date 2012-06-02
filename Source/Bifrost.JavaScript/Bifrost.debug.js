5c7a61c9d340671f2de9f2448557cd229e07dbb7
Einar Ingebrigtsen <einari@Einar-Ingebrigtsens-iMac.local>
2012-06-02 19:05:51 +0200

    New Debug version of Bifrost

--- Source/Bifrost.JavaScript/Bifrost.debug.js
+++ Source/Bifrost.JavaScript/Bifrost.debug.js
@@ -1345,20 +1345,20 @@ if (typeof ko !== 'undefined') {
 Bifrost.namespace("Bifrost.navigation", {
 	navigationManager: {
 		hookup: function(parent) {
-			var result = $("[data-navigate-to]", parent);
-            $("*[data-navigate-to]", parent).each(function () {
-				var target = $(this).data("navigate-to");
+            $("a", parent).each(function (index, item) {
+				var target = item.href.replace("file://","");
+				if( target.indexOf("/") === 0 ) {
+					while( target.indexOf("/") == 0 ) {
+						target = target.substr(1);
+					}
 				
-				while( target.indexOf("/") == 0 ) {
-					target = target.substr(1);
+					$(this).attr("href","/"+target);
+				
+					$(this).bind("click", function(e) {
+						e.preventDefault();
+						History.pushState({},"NO TITLE AT THE MOMENT","/"+target);
+					});
 				}
-				
-				$(this).attr("href","/"+target);
-				
-				$(this).bind("click", function(e) {
-					e.preventDefault();
-					History.pushState({},"NO TITLE AT THE MOMENT","/"+target);
-				});
 			});
 		}
 	}
