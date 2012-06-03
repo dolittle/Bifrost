581fae129ba88210c51fbcc3acac2058b049b3cc
Einar Ingebrigtsen <einari@Einar-Ingebrigtsens-iMac.local>
2012-06-03 10:22:35 +0200

    Greatest and latest

--- Source/Bifrost.JavaScript/navigation/navigationManager.js
+++ Source/Bifrost.JavaScript/navigation/navigationManager.js
@@ -2,7 +2,11 @@ Bifrost.namespace("Bifrost.navigation", {
 	navigationManager: {
 		hookup: function(parent) {
             $("a", parent).each(function (index, item) {
-				var targetUri = Bifrost.Uri.create(item.href);
+				var href = item.href;
+				if( href.length == 0 ) {
+					href = "/";
+				}
+				var targetUri = Bifrost.Uri.create(href);
 				if( targetUri.isSameAsOrigin ) {
 					var target = targetUri.path;
 					while( target.indexOf("/") == 0 ) {
