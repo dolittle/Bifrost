a345b30721b1e1ca17af7c86ae512cf2ed26f5b5
Einar Ingebrigtsen <einar@dolittle.com>
2012-06-07 15:03:43 +0200

    Renaming CommandHelper to be CommandHtmlHelper and introducing an Ajax variant.

---
+++ Source/Bifrost.Web.Mvc/Commands/CommandAjaxHelper.cs
@@ -0,0 +1,192 @@
+﻿#region License
+//
+// Copyright (c) 2008-2012, DoLittle Studios AS and Komplett ASA
+//
+// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
+// With one exception :
+//   Commercial libraries that is based partly or fully on Bifrost and is sold commercially,
+//   must obtain a commercial license.
+//
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the license at
+//
+//   http://bifrost.codeplex.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Bifrost.Commands;
+using System.Web.Mvc.Ajax;
+using System.Globalization;
+using System.Text;
+
+namespace Bifrost.Web.Mvc.Commands
+{
+    /// <summary>
+    /// Provides Ajax helper methods for Commands
+    /// </summary>
+    public static class CommandAjaxHelper
+    {
+        /// <summary>
+        /// Begins a <see cref="CommandForm{T}"/>, with default <see cref="FormMethod.Post"/> as method
+        /// </summary>
+        /// <typeparam name="T">Type of Command to create form for</typeparam>
+        /// <typeparam name="TC">Type of controller holding the action to forward to</typeparam>
+        /// <param name="AjaxHelper">AjaxHelper to begin a command form within</param>
+        /// <param name="ajaxOptions">Ajax Options for the command form</param>
+        /// <param name="expression">Expression holding information about the action on the controller to use</param>
+        /// <returns>A <see cref="CommandForm{T}"/></returns>
+        /// <remarks>
+        /// For the expression that expressed the action to use, it does not care about the parameters for the action, so these
+        /// can be set to null. The expression just represents the action strongly typed.
+        /// </remarks>
+        public static CommandForm<T> BeginCommandForm<T, TC>(this AjaxHelper AjaxHelper, Expression<Func<TC, ActionResult>> expression, AjaxOptions ajaxOptions)
+            where T : ICommand, new()
+            where TC : ControllerBase
+        {
+            var command = AjaxHelper.BeginCommandForm<T, TC>(expression, ajaxOptions, new Dictionary<string, object>());
+            return command;
+        }
+
+
+        /// <summary>
+        /// Begins a <see cref="CommandForm{T}"/>, with default <see cref="FormMethod.Post"/> as method
+        /// </summary>
+        /// <typeparam name="T">Type of Command to create form for</typeparam>
+        /// <typeparam name="TC">Type of controller holding the action to forward to</typeparam>
+        /// <param name="AjaxHelper">AjaxHelper to begin a command form within</param>
+        /// <param name="ajaxOptions">Ajax Options for the command form</param>
+        /// <param name="expression">Expression holding information about the action on the controller to use</param>
+        /// <param name="htmlAttributes">An object that contains the HTML attributes to be set for the element</param>
+        /// <returns>A <see cref="CommandForm{T}"/></returns>
+        /// <remarks>
+        /// For the expression that expressed the action to use, it does not care about the parameters for the action, so these
+        /// can be set to null. The expression just represents the action strongly typed.
+        /// </remarks>
+        public static CommandForm<T> BeginCommandForm<T, TC>(this AjaxHelper AjaxHelper, Expression<Func<TC, ActionResult>> expression, AjaxOptions ajaxOptions, object htmlAttributes)
+            where T : ICommand, new()
+            where TC : ControllerBase
+        {
+            var command = AjaxHelper.BeginCommandForm<T, TC>(expression, ajaxOptions, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return command;
+        }
+
+        /// <summary>
+        /// Begins a CommandForm
+        /// </summary>
+        /// <typeparam name="T">Type of Command to create form for</typeparam>
+        /// <typeparam name="TC">Type of controller holding the action to forward to</typeparam>
+        /// <param name="AjaxHelper">AjaxHelper to begin a command form within</param>
+        /// <param name="ajaxOptions">Ajax Options for the command form</param>
+        /// <param name="expression">Expression holding information about the action on the controller to use</param>
+        /// <param name="htmlAttributes">An object that contains the HTML attributes to be set for the element</param>
+        /// <returns>A <see cref="CommandForm{T}"/></returns>
+        /// <remarks>
+        /// For the expression that expressed the action to use, it does not care about the parameters for the action, so these
+        /// can be set to null. The expression just represents the action strongly typed.
+        /// </remarks>
+        public static CommandForm<T> BeginCommandForm<T, TC>(this AjaxHelper AjaxHelper, Expression<Func<TC, ActionResult>> expression, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+            where T : ICommand, new()
+            where TC : ControllerBase
+        {
+            var lambda = expression as LambdaExpression;
+            if( null != lambda )
+            {
+                var methodCallExpression = lambda.Body as MethodCallExpression;
+                if( null != methodCallExpression )
+                {
+                    var actionName = methodCallExpression.Method.Name;
+                    var controllerName = typeof (TC).Name.Replace("Controller",string.Empty);
+                    var commandForm = BeginCommandForm<T>(AjaxHelper, actionName, controllerName, ajaxOptions, htmlAttributes);
+                    return commandForm;
+                }
+            }
+
+            return null;
+        }
+
+
+        /// <summary>
+        /// Begins a CommandForm
+        /// </summary>
+        /// <typeparam name="T">Type of Command to create form for</typeparam>
+        /// <param name="AjaxHelper">AjaxHelper to begin a command form within</param>
+        /// <param name="actionName">Action to call on the controller</param>
+        /// <param name="controllerName">Controller the action belongs to</param>
+        /// <param name="ajaxOptions">Ajax Options for the command form</param>
+        /// <returns>A <see cref="CommandForm{T}"/></returns>
+        public static CommandForm<T> BeginCommandForm<T>(this AjaxHelper AjaxHelper, string actionName, string controllerName, AjaxOptions ajaxOptions)
+            where T : ICommand, new()
+        {
+            return AjaxHelper.BeginCommandForm<T>(actionName, controllerName, ajaxOptions, new Dictionary<string, object>());
+        }
+
+        /// <summary>
+        /// Begins a CommandForm
+        /// </summary>
+        /// <typeparam name="T">Type of Command to create form for</typeparam>
+        /// <param name="AjaxHelper">AjaxHelper to begin a command form within</param>
+        /// <param name="actionName">Action to call on the controller</param>
+        /// <param name="controllerName">Controller the action belongs to</param>
+        /// <param name="ajaxOptions">Ajax Options for the command form</param>
+        /// <param name="htmlAttributes">An object that contains the HTML attributes to be set for the element</param>
+        /// <returns>A <see cref="CommandForm{T}"/></returns>
+        public static CommandForm<T> BeginCommandForm<T>(this AjaxHelper AjaxHelper, string actionName, string controllerName, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+            where T : ICommand, new()
+        {
+            var formAction = UrlHelper.GenerateUrl(null, actionName, controllerName, new RouteValueDictionary(), AjaxHelper.RouteCollection, AjaxHelper.ViewContext.RequestContext, true);
+            var form = FormHelper<T>(AjaxHelper, formAction, ajaxOptions, htmlAttributes);
+            return form;
+        }
+
+        /// <summary>
+        /// Begins a CommandForm
+        /// </summary>
+        /// <typeparam name="T">Type of Command to create form for</typeparam>
+        /// <param name="AjaxHelper">AjaxHelper to begin a command form within</param>
+        /// <param name="actionName">Action to call on the controller</param>
+        /// <param name="controllerName">Controller the action belongs to</param>
+        /// <param name="ajaxOptions">Ajax Options for the command form</param>
+        /// <param name="htmlAttributes">An object that contains the HTML attributes to be set for the element</param>
+        /// <returns>A <see cref="CommandForm{T}"/></returns>
+        public static CommandForm<T> BeginCommandForm<T>(this AjaxHelper AjaxHelper, string actionName, string controllerName, AjaxOptions ajaxOptions, object htmlAttributes)
+           where T : ICommand, new()
+        {
+            return AjaxHelper.BeginCommandForm<T>(actionName, controllerName, ajaxOptions, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        static CommandForm<T> FormHelper<T>(this AjaxHelper ajaxHelper, string formAction, AjaxOptions ajaxOptions, IDictionary<string, object> htmlAttributes)
+            where T : ICommand, new()
+        {
+            htmlAttributes = htmlAttributes ?? new Dictionary<string, object>();
+
+            var builder = new TagBuilder("form");
+            builder.MergeAttribute("action", formAction);
+            builder.MergeAttributes<string, object>(htmlAttributes);
+            builder.MergeAttribute("method", "post");
+            builder.MergeAttribute("data-commandForm","",true);
+            ajaxOptions = ajaxOptions ?? new AjaxOptions();
+            if (ajaxHelper.ViewContext.UnobtrusiveJavaScriptEnabled)
+                builder.MergeAttributes<string, object>(ajaxOptions.ToUnobtrusiveHtmlAttributes());
+
+            if (ajaxHelper.ViewContext.ClientValidationEnabled && !htmlAttributes.ContainsKey("id"))
+                builder.GenerateId(typeof (T).Name);
+            
+            ajaxHelper.ViewContext.Writer.Write(builder.ToString(TagRenderMode.StartTag));
+            var form = new CommandForm<T>(ajaxHelper.ViewContext);
+            if (ajaxHelper.ViewContext.ClientValidationEnabled)
+                ajaxHelper.ViewContext.FormContext.FormId = builder.Attributes["id"];
+            return form;
+        }
+
+    }
+}
