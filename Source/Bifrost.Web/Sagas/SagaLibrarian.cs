cb16d511cec2cdbaf266ecfb5c570605f0fc869a
Christian Lundestad <christian@lundestad.com>
2012-06-20 14:02:35 +0200

    Improved TypeExtensions.HasDefaultConstructor. Reinstated serialization of sagas to session.

--- Source/Bifrost.Web/Sagas/SagaLibrarian.cs
+++ Source/Bifrost.Web/Sagas/SagaLibrarian.cs
@@ -27,48 +27,46 @@ using Bifrost.Sagas;
 
 namespace Bifrost.Web.Sagas
 {
-	public class SagaLibrarian : ISagaLibrarian
-	{
-        //readonly ISagaConverter _sagaConverter;
+    public class SagaLibrarian : ISagaLibrarian
+    {
+        readonly ISagaConverter _sagaConverter;
 
-        //public SagaLibrarian(ISagaConverter sagaConverter)
-        //{
-        //    _sagaConverter = sagaConverter;
-        //}
+        public SagaLibrarian(ISagaConverter sagaConverter)
+        {
+            _sagaConverter = sagaConverter;
+        }
 
+        public void Close(ISaga saga)
+        {
+            HttpContext.Current.Session.Remove(saga.Id.ToString());
+        }
 
-		public void Close(ISaga saga)
-		{
-			HttpContext.Current.Session.Remove(saga.Id.ToString());
-		}
+        public void Catalogue(ISaga saga)
+        {
+            var sagaHolder = _sagaConverter.ToSagaHolder(saga);
+            HttpContext.Current.Session[saga.Id.ToString()] = sagaHolder;
+        }
 
-		public void Catalogue(ISaga saga)
-		{
-			//var sagaHolder = _sagaConverter.ToSagaHolder(saga);
-			//HttpContext.Current.Session[saga.Id.ToString()] = sagaHolder;
-            HttpContext.Current.Session[saga.Id.ToString()] = saga;
-		}
-
-		public ISaga Get(Guid id)
-		{
-            //var sagaInSession = HttpContext.Current.Session[id.ToString()];
-            var sagaInSession = HttpContext.Current.Session[id.ToString()] as ISaga;
+        public ISaga Get(Guid id)
+        {
+            var sagaInSession = HttpContext.Current.Session[id.ToString()];
             var exists = sagaInSession != null;
-			return !exists ? null : sagaInSession;
+            if (!exists)
+                return null;
 
-		    //var sagaHolder = sagaInSession as SagaHolder;
-		    //var saga = _sagaConverter.ToSaga(sagaHolder);
-		    //return saga;
-		}
+            var sagaHolder = sagaInSession as SagaHolder;
+            var saga = _sagaConverter.ToSaga(sagaHolder);
+            return saga;
+        }
 
-		public ISaga Get(string partition, string key)
-		{
-			throw new NotImplementedException();
-		}
+        public ISaga Get(string partition, string key)
+        {
+            throw new NotImplementedException();
+        }
 
-		public IEnumerable<ISaga> GetForPartition(string partition)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public IEnumerable<ISaga> GetForPartition(string partition)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
