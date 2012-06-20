c2744ed371979cc1adea1b117edfe44507c7f022
Christian Lundestad <christian@lundestad.com>
2012-06-20 13:58:50 +0200

    Improved TypeExtensions.HasDefaultConstructor

--- Source/Bifrost/Extensions/TypeExtensions.cs
+++ Source/Bifrost/Extensions/TypeExtensions.cs
@@ -20,24 +20,14 @@
 //
 #endregion
 using System;
-using System.Reflection;
 
 namespace Bifrost.Extensions
 {
-	/// <summary>
-	/// Provides a set of methods for working with <see cref="Type">types</see>
-	/// </summary>
-	public static class TypeExtensions
-	{
-#pragma warning disable 1591 // Xml Comments
-        static ITypeInfo GetTypeInfo(Type type)
-        {
-            var typeInfoType = typeof(TypeInfo<>).MakeGenericType(type);
-            return typeInfoType.GetField("Instance", BindingFlags.Public | BindingFlags.Static).GetValue(null) as ITypeInfo;
-        }
-#pragma warning restore 1591 // Xml Comments
-
-
+    /// <summary>
+    /// Provides a set of methods for working with <see cref="Type">types</see>
+    /// </summary>
+    public static class TypeExtensions
+    {
         /// <summary>
         /// Check if a type has a default constructor that does not take any arguments
         /// </summary>
@@ -45,20 +35,23 @@ namespace Bifrost.Extensions
         /// <returns>true if it has a default constructor, false if not</returns>
         public static bool HasDefaultConstructor(this Type type)
         {
-            return GetTypeInfo(type).HasDefaultConstructor;
+            if (type.IsValueType)
+                return true;
+
+            return type.GetConstructor(Type.EmptyTypes) != null;
         }
 
-		/// <summary>
-		/// Check if a type implements a specific interface
-		/// </summary>
-		/// <typeparam name="T">Interface to check for</typeparam>
-		/// <param name="type">Type to check</param>
-		/// <returns>True if the type implements the interface, false if not</returns>
-		public static bool HasInterface<T>(this Type type)
-		{
-		    var hasInterface = type.HasInterface(typeof (T));
-			return hasInterface;
-		}
+        /// <summary>
+        /// Check if a type implements a specific interface
+        /// </summary>
+        /// <typeparam name="T">Interface to check for</typeparam>
+        /// <param name="type">Type to check</param>
+        /// <returns>True if the type implements the interface, false if not</returns>
+        public static bool HasInterface<T>(this Type type)
+        {
+            var hasInterface = type.HasInterface(typeof(T));
+            return hasInterface;
+        }
 
         /// <summary>
         /// Check if a type implements a specific interface
@@ -72,5 +65,5 @@ namespace Bifrost.Extensions
             return hasInterface;
         }
 
-	}
+    }
 }
