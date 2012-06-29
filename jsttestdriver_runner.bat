556b3ec57ce4170bd7dc7639d327aff09169fbc7
Marius Gundersen <gundersen@gmail.com>
2012-06-29 10:44:53 +0200

    moved jsTestdriver.conf out of Source (it's not sourcecode). Added runner and server script for windows

---
+++ jsttestdriver_runner.bat
@@ -0,0 +1,2 @@
+@java -jar Tools\jstestdriver\JsTestDriver-1.3.4-a.jar --config jsTestDriver.conf --tests all --reset
+@pause
---
+++ jsttestdriver_server.bat
@@ -0,0 +1,2 @@
+@echo Navigate to http://localhost:9876 and click on "Capture This Browser"!
+@java -jar Tools\jstestdriver\JsTestDriver-1.3.4-a.jar --config jsTestDriver.conf --port 9876
