a728397ed23791cb9eff7a6af14e572029def431
Einar Ingebrigtsen <einar@dolittle.com>
2012-07-25 22:58:47 +0200

    Moved namespace function out of its closure

--- Source/Bifrost.JavaScript/Utils/namespace.js
+++ Source/Bifrost.JavaScript/Utils/namespace.js
@@ -1,17 +1,15 @@
 var Bifrost = Bifrost || {};
-(function(global, undefined) {
-    Bifrost.namespace = function (ns, content) {
-        var parent = global;
-        var parts = ns.split('.');
-        $.each(parts, function (index, part) {
-            if (!Object.prototype.hasOwnProperty.call(parent, part)) {
-                parent[part] = {};
-            }
-            parent = parent[part];
-        });
+Bifrost.namespace = function (ns, content) {
+    var parent = window;
+    var parts = ns.split('.');
+    $.each(parts, function (index, part) {
+        if (!Object.prototype.hasOwnProperty.call(parent, part)) {
+            parent[part] = {};
+        }
+        parent = parent[part];
+    });
 
-		if( typeof content === "object" ) {
-			Bifrost.extend(parent, content);
-		}
-    };
-})(window);
+	if( typeof content === "object" ) {
+		Bifrost.extend(parent, content);
+	}
+};
