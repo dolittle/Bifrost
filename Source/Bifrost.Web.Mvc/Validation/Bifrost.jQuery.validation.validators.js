519b1b9aef55351aa916feed595958163a62ac49
Einar Ingebrigtsen <einar@dolittle.com>
2012-08-10 14:23:16 +0200

    Adding jQuery Unobtrusive Validation client side support for common validators

---
+++ Source/Bifrost.Web.Mvc/Validation/Bifrost.jQuery.validation.validators.js
@@ -0,0 +1,31 @@
+﻿$.validator.addMethod("greaterthan", function (value, element, params) {
+    return value > params;
+});
+$.validator.unobtrusive.adapters.add("greaterthan", ["valuetocompare"], function (options) {
+    options.rules["greaterthan"] = options.params.valuetocompare;
+    options.messages["greaterthan"] = options.message;
+});
+
+$.validator.addMethod("greaterthanorequal", function (value, element, params) {
+    return value > params;
+});
+$.validator.unobtrusive.adapters.add("greaterthanorequal", ["valuetocompare"], function (options) {
+    options.rules["greaterthanorequal"] = options.params.valuetocompare;
+    options.messages["greaterthanorequal"] = options.message;
+});
+
+$.validator.addMethod("lessthan", function (value, element, params) {
+    return value < params;
+});
+$.validator.unobtrusive.adapters.add("lessthan", ["valuetocompare"], function (options) {
+    options.rules["lessthan"] = options.params.valuetocompare;
+    options.messages["lessthan"] = options.message;
+});
+
+$.validator.addMethod("lessthanorequal", function (value, element, params) {
+    return value < params;
+});
+$.validator.unobtrusive.adapters.add("lessthanorequal", ["valuetocompare"], function (options) {
+    options.rules["lessthanorequal"] = options.params.valuetocompare;
+    options.messages["lessthanorequal"] = options.message;
+});
