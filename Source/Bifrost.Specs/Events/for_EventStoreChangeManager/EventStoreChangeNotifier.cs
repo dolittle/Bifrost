d190ae3373e2364ded5743270ef2d67df2a87370
Einar Ingebrigtsen <einar@dolittle.com>
2012-09-28 16:40:28 +0200

    Updated specs to take into consideration the new event stream that the change notifiers are receiving

--- Source/Bifrost.Specs/Events/for_EventStoreChangeManager/EventStoreChangeNotifier.cs
+++ Source/Bifrost.Specs/Events/for_EventStoreChangeManager/EventStoreChangeNotifier.cs
@@ -6,10 +6,13 @@ namespace Bifrost.Specs.Events.for_EventStoreChangeManager
     {
         public bool NotifyCalled = false;
         public IEventStore EventStore = null;
-        public void Notify(IEventStore eventStore)
+        public EventStream EventStream = null;
+
+        public void Notify(IEventStore eventStore, EventStream streamOfEvents)
         {
             NotifyCalled = true;
             EventStore = eventStore;
+            EventStream = streamOfEvents;
         }
     }
 }
--- Source/Bifrost.Specs/Events/for_EventStoreChangeManager/when_notifying_changes_with_one_notifier.cs
+++ Source/Bifrost.Specs/Events/for_EventStoreChangeManager/when_notifying_changes_with_one_notifier.cs
@@ -3,6 +3,8 @@ using Moq;
 using Machine.Specifications;
 using It = Machine.Specifications.It;
 using Bifrost.Events;
+using System;
+using Bifrost.Fakes.Events;
 
 
 namespace Bifrost.Specs.Events.for_EventStoreChangeManager
@@ -11,12 +13,20 @@ namespace Bifrost.Specs.Events.for_EventStoreChangeManager
     public class when_notifying_changes_with_one_notifier : given.an_event_store_change_manager_with_one_notifier_registered
     {
         static Mock<IEventStore>    event_store_mock;
+        static UncommittedEventStream stream_of_events;
 
-        Establish context = () => event_store_mock = new Mock<IEventStore>();
+        Establish context = () => 
+        { 
+            event_store_mock = new Mock<IEventStore>();
+            stream_of_events = new UncommittedEventStream(Guid.NewGuid());
+            stream_of_events.Append(new SimpleEvent(Guid.NewGuid()));
+            stream_of_events.Append(new SimpleEvent(Guid.NewGuid()));
+        };
 
-        Because of = () => event_store_change_manager.NotifyChanges(event_store_mock.Object);
+        Because of = () => event_store_change_manager.NotifyChanges(event_store_mock.Object, stream_of_events);
 
         It should_forward_notification_to_the_notifier = () => notifier.NotifyCalled.ShouldBeTrue();
         It should_forward_event_store_to_the_notifier_during_notification = () => notifier.EventStore.ShouldEqual(event_store_mock.Object);
+        It should_forward_stream_of_events = () => notifier.EventStream.ShouldEqual(stream_of_events);
     }
 }
