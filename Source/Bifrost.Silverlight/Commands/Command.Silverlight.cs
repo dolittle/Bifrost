b2151f034324a959f328b543f47b431361a12bf7
Einar Ingebrigtsen <einar@dolittle.com>
2012-10-05 19:34:13 +0200

    Making Command a partial class so we can do specific client implementations for platforms such as Silverlight

---
+++ Source/Bifrost.Silverlight/Commands/Command.Silverlight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bifrost.Commands
+{
+    public partial class Command : System.Windows.Input.ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged = (s, e) => { };
+
+        public void Execute(object parameter)
+        {
+            var i = 0;
+            i++;
+            
+        }
+    }
+}
