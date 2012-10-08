112e7f0d3aca2db89a978190a670c836560a2474
Einar Ingebrigtsen <einar@dolittle.com>
2012-10-08 15:43:05 +0200

    Adding support for the dependency resolver for SignalR

---
+++ Source/Bifrost.SignalR/BifrostDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bifrost.Execution;
+using SignalR;
+
+namespace Bifrost.SignalR
+{
+    public class BifrostDependencyResolver : DefaultDependencyResolver
+    {
+        IContainer _container;
+        public BifrostDependencyResolver(IContainer container)
+        {
+            _container = container;
+        }
+
+        public override object GetService(Type serviceType)
+        {
+            try
+            {
+                return _container.Get(serviceType);
+            }
+            catch
+            {
+                return base.GetService(serviceType);
+            }
+        }
+
+        public override IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return _container.GetAll(serviceType).Concat(base.GetServices(serviceType));
+            }
+            catch
+            {
+                return base.GetServices(serviceType);
+            }
+        }
+    }
+}
