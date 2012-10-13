53ed300ac22cc81b2388b1f3e9277141ec9a8df1
Einar Ingebrigtsen <einar@dolittle.com>
2012-10-13 11:31:57 +0200

    Started fleshing out some of the domain for Mimir

---
+++ Source/Bifrost.Mimir.Domain/EventSubscribers/EventSubscriber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Bifrost.Domain;
+
+namespace Bifrost.Mimir.Domain.EventSubscribers
+{
+    public class EventSubscriber : AggregatedRoot
+    {
+        public EventSubscriber(Guid id)
+            : base(id)
+        {
+        }
+    }
+}
---
+++ Source/Bifrost.Mimir.Events/EventSubscribers/EventSubscriberVersionResetted.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Bifrost.Events;
+
+namespace Bifrost.Mimir.Events.EventSubscribers
+{
+    public class EventSubscriberVersionResetted : Event
+    {
+        public EventSubscriberVersionResetted(Guid eventSourceId)
+            : base(eventSourceId)
+        {
+        }
+
+        public long EventId { get; set; }
+    }
+}
