0fcbd9d2ee2c327b05eea147eba5b7b2ccaa3c8e
Einar Ingebrigtsen <einar@dolittle.com>
2012-10-14 14:57:58 +0200

    Started implementing the ability to "host" Web apps from DLLs

---
+++ Source/Bifrost.Mimir.Web/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Routing;
+using Bifrost.Web.Applications;
+
+namespace Bifrost.Mimir.Web
+{
+    public class ClassForTypeSafeConfiguration
+    {
+    }
+}
+
+namespace Bifrost.Configuration
+{
+    
+
+    public static class ConfigurationExtensions
+    {
+        public static IConfigure WithMimir(this IConfigure configure)
+        {
+            RouteTable.Routes.AddApplicationFromAssembly("Mimir", typeof(Bifrost.Mimir.Web.ClassForTypeSafeConfiguration).Assembly);
+            return configure;
+        }
+    }
+}
--- Source/Bifrost.Web/Applications/ApplicationRouteHttpHandler.cs
+++ Source/Bifrost.Web/Applications/ApplicationRouteHttpHandler.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace Bifrost.Web.Applications
@@ -7,17 +10,46 @@ namespace Bifrost.Web.Applications
     {
         string _url;
         Assembly _assembly;
+        Dictionary<string, Stream> _resources;
 
         public ApplicationRouteHttpHandler(string url, Assembly assembly)
         {
             _url = url;
             _assembly = assembly;
+            var resources = _assembly.GetManifestResourceNames();
+            _resources = new Dictionary<string,Stream>();
+            foreach (var resource in resources)
+                _resources[GetRelativePathFromResourceName(resource)] = _assembly.GetManifestResourceStream(resource);
+
+        }
+
+        string GetRelativePathFromResourceName(string resourceName)
+        {
+            resourceName = resourceName.Replace(_assembly.GetName().Name + ".", string.Empty);
+            resourceName = resourceName.Replace(".", "/");
+            var indexOfSlash = resourceName.LastIndexOf("/");
+            var actualResourceName = string.Format("{0}.{1}",
+                resourceName.Substring(0, indexOfSlash),
+                resourceName.Substring(indexOfSlash+1)
+                );
+            return actualResourceName;
         }
 
         public bool IsReusable { get { return true; } }
 
         public void ProcessRequest(HttpContext context)
         {
+            var url = context.Request.Url.AbsolutePath.Replace("/"+_url,string.Empty);
+            if (string.IsNullOrEmpty(url))
+                url = "index.html";
+
+            if (!_resources.ContainsKey(url))
+                return;
+
+            var resource = _resources[url];
+            var bytes = new byte[resource.Length];
+            resource.Read(bytes,0,bytes.Length);
+            context.Response.OutputStream.Write(bytes, 0, bytes.Length);
         }
     }
 }
