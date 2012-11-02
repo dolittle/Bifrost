24b733303e4520f7c32fb8216dfb6c6b997ce1da
Einar Ingebrigtsen <einar@dolittle.com>
2012-11-02 15:11:32 +0100

    Current namespace set on content within the namespace - making it easy to know where one is

--- Source/Bifrost.JavaScript/utils/namespace.js
+++ Source/Bifrost.JavaScript/utils/namespace.js
@@ -1,16 +1,30 @@
 var Bifrost = Bifrost || {};
 Bifrost.namespace = function (ns, content) {
     var parent = window;
+    var name = "";
     var parts = ns.split('.');
     $.each(parts, function (index, part) {
+        if (name.length > 0) {
+            name += ".";
+        }
+        name += part;
         if (!Object.prototype.hasOwnProperty.call(parent, part)) {
             parent[part] = {};
             parent[part].parent = parent;
+            parent[part].name = name;
         }
         parent = parent[part];
     });
 
     if (typeof content === "object") {
+        Bifrost.namespace.current = parent;
         Bifrost.extend(parent, content);
+
+        for (var property in parent) {
+            if (parent.hasOwnProperty(property)) {
+                parent[property]._namespace = parent;
+            }
+        }
+        Bifrost.namespace.current = null;
     }
 };
