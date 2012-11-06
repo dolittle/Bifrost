ba881d1fb7149481b7261497f4a3669f8d964e57
Einar Ingebrigtsen <einar@dolittle.com>
2012-11-06 12:59:22 +0100

    Refactoring the namespaces to have configurable conventions - this is just a temporary solution, till we bring on the real guns

--- Source/Bifrost.JavaScript/utils/namespaces.js
+++ Source/Bifrost.JavaScript/utils/namespaces.js
@@ -1,34 +1,54 @@
 Bifrost.namespace("Bifrost", {
-	namespaces: {
-		root: "/",
-		initialize: function() {		
-			var scripts = Bifrost.assetsManager.getScripts();
+    namespaces: (function () {
+        var self = this;
+        this.conventions = [];
+
+        this.addConvention = function (path, namespace) {
+            path = self.stripPath(path);
+            self.conventions.push({
+                path: path,
+                namespace: namespace
+            });
+        };
+
+        this.stripPath = function (path) {
+            if (path.startsWith("/")) {
+                path = path.substr(1);
+            }
+            if (path.endsWith("/")) {
+                path = path.substr(0, path.length - 1);
+            }
+            return path;
+        };
+
+        this.initialize = function () {
+            var scripts = Bifrost.assetsManager.getScripts();
             $.each(scripts, function (index, fullPath) {
                 var path = Bifrost.path.getPathWithoutFilename(fullPath);
-                if( path.startsWith("/") ) {
-                	path = path.substr(1);
-                }
-			
-                var namespacePath = path.split("/").join(".");
-                var namespace = Bifrost.namespace(namespacePath);
-                var actualRoot = Bifrost.namespaces.root;
-                if( !actualRoot.endsWith("/") ) {
-                	actualRoot += "/";
-                }
+                path = self.stripPath(path);
+                $.each(self.conventions, function (conventionIndex, convention) {
+                    if (path.startsWith(convention.path)) {
+                        var namespacePath = path.split("/").join(".");
+                        if (convention.namespace.length > 0) {
+                            namespacePath = convention.namespace + "." + namespacePath;
+                        }
+                        var namespace = Bifrost.namespace(namespacePath);
+                        var root = "/" + path + "/";
+                        namespace._path = root;
 
-                namespace._path = actualRoot + path;
+                        if (typeof namespace._scripts === "undefined") {
+                            namespace._scripts = [];
+                        }
 
-                if( typeof namespace._scripts === "undefined" ) {
-                	namespace._scripts = [];
-                }
+                        var fileIndex = fullPath.lastIndexOf("/");
+                        var file = fullPath.substr(fileIndex + 1);
+                        var extensionIndex = file.lastIndexOf(".");
+                        var system = file.substr(0, extensionIndex);
 
-                var fileIndex = fullPath.lastIndexOf("/");
-                var file = fullPath.substr(fileIndex+1);
-                var extensionIndex = file.lastIndexOf(".");
-                var system = file.substr(0,extensionIndex);
-
-                namespace._scripts.push(system);
+                        namespace._scripts.push(system);
+                    }
+                });
             });
-		}
-	}
+        };
+    })()
 });
