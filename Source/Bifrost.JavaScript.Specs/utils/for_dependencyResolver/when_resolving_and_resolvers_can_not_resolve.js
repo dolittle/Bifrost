8bb74868060416575638d00cef98dc3c9604cd90
Einar Ingebrigtsen <einar@dolittle.com>
2012-11-16 18:53:58 +0100

    Specification for proving what should happen when we are not able to resolve a dependency

---
+++ Source/Bifrost.JavaScript.Specs/utils/for_dependencyResolver/when_resolving_and_resolvers_can_not_resolve.js
@@ -0,0 +1,24 @@
+﻿describe("when resolving and resolvers can not resolve", function () {
+    var resolver = {
+        canResolve: sinon.stub().returns(false)
+    };
+    var exception;
+
+    beforeEach(function () {
+        Bifrost.dependencyResolvers = {
+            getAll: function () {
+                return [resolver];
+            }
+        };
+        try {
+            Bifrost.dependencyResolver.resolve("Something");
+        } catch (e) {
+            exception = e;
+        }
+    });
+
+    it("should throw unresolved dependencies exception", function () {
+        expect(exception instanceof Bifrost.UnresolvedDependencies).toBeTruthy();
+    });
+
+});
