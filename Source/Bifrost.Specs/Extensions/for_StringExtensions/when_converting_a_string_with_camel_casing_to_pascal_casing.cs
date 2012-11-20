a3d327ae777685512e55fdbc2f3141cfaf61bd1d
Einar Ingebrigtsen <einar@dolittle.com>
2012-11-20 20:29:36 +0100

    Putting in support for converting to pascal case + adding specs to prove it :)

---
+++ Source/Bifrost.Specs/Extensions/for_StringExtensions/when_converting_a_string_with_camel_casing_to_pascal_casing.cs
@@ -0,0 +1,14 @@
+﻿using Bifrost.Extensions;
+using Machine.Specifications;
+
+namespace Bifrost.Specs.Extensions.for_StringExtensions
+{
+    public class when_converting_a_string_with_camel_casing_to_pascal_casing
+    {
+        static string result;
+
+        Because of = () => result = "camelCased".ToPascalCase();
+
+        It should_turn_it_into_pascal_case = () => result.ShouldEqual("CamelCased");
+    }
+}
