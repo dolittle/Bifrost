0ec6bb8196a370fdf947c7f0db535378fa6d55e8
Einar Ingebrigtsen <einar@dolittle.com>
2012-12-06 18:54:50 +0100

    Introducing a Knockout specific View and ViewEngine - supporting BifrostJS

---
+++ Source/Bifrost.Web.Mvc/Views/KnockoutRazorView.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Bifrost.Web.Mvc.Views
+{
+    public class KnockoutRazorView : RazorView
+    {
+        string _viewName;
+        bool _isPartial;
+
+        public KnockoutRazorView(
+            ControllerContext controllerContext,
+            string viewName,
+            string layoutPath,
+            bool isPartial,
+            bool runViewStartPages,
+            IEnumerable<string> viewStartFileExtensions)
+            : base(controllerContext, GetPathFromRoute(viewName, "cshtml", controllerContext.RouteData), layoutPath, runViewStartPages, viewStartFileExtensions)
+        {
+            _isPartial = isPartial;
+            _viewName = viewName;
+        }
+
+        public static bool ViewExists(HttpContextBase httpContext, string viewName, RouteData routeData)
+        {
+            var relativePath = GetPathFromRoute(viewName, "cshtml", routeData);
+            var path = httpContext.Server.MapPath(relativePath);
+            return File.Exists(path);
+        }
+
+        static string GetPathFromRoute(string name, string extension, RouteData routeData)
+        {
+            var relativePath = string.Format("{0}/{1}.{2}",
+                GetFeaturePathFrom(routeData),
+                name,
+                extension);
+            return relativePath;
+        }
+
+        static string GetFeaturePathFrom(RouteData routeData)
+        {
+            var relativePath = string.Format("/Contexts/{0}/{1}/{2}",
+                routeData.Values["boundedContext"],
+                routeData.Values["module"],
+                routeData.Values["feature"]);
+            return relativePath;
+        }
+
+        static string GetNamespaceFrom(RouteData routeData)
+        {
+            return string.Format("{0}.{1}.{2}",
+                routeData.Values["boundedContext"],
+                routeData.Values["module"],
+                routeData.Values["feature"]);
+        }
+
+        protected override void RenderView(ViewContext viewContext, TextWriter writer, object instance)
+        {
+            var featureRelativePath = GetFeaturePathFrom(viewContext.RouteData);
+            var featurePath = viewContext.HttpContext.Server.MapPath(featureRelativePath);
+            var featureNamespace = GetNamespaceFrom(viewContext.RouteData);
+            var viewModelRelativePath = GetPathFromRoute(_viewName, "js", viewContext.RouteData);
+            var viewModelPath = viewContext.HttpContext.Server.MapPath(viewModelRelativePath);
+            var hasViewModel = File.Exists(viewModelPath);
+            var divTagId = string.Format("{0}_ViewModel", _viewName);
+
+            if (hasViewModel)
+                writer.WriteLine("<div id=\"{0}\" data-viewmodel=\"{1}\">",
+                    divTagId,
+                    viewModelRelativePath);
+
+            base.RenderView(viewContext, writer, instance);
+
+            if (hasViewModel)
+                writer.WriteLine("</div>");
+        }
+    }
+}
---
+++ Source/Bifrost.Web.Mvc/Views/KnockoutRazorViewEngine.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using System.IO;
+
+namespace Bifrost.Web.Mvc.Views
+{
+    public class KnockoutRazorViewEngine : RazorViewEngine
+    {
+        public override ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            var result = base.FindPartialView(controllerContext, partialViewName, useCache);
+            if (result.View == null)
+                result = GetPartialViewByConvention(controllerContext, partialViewName, useCache) ?? result;
+            return result;
+        }
+
+        public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            var result = base.FindView(controllerContext, viewName, masterName, useCache);
+            if (result.View == null)
+                result = GetViewByConvention(controllerContext, viewName, useCache) ?? result;
+            return result;
+        }
+
+        ViewEngineResult GetPartialViewByConvention(ControllerContext controllerContext, string viewName, bool useCache)
+        {
+            return GetViewByConventionImpl(controllerContext, viewName, useCache, true, false);
+        }
+
+        ViewEngineResult GetViewByConvention(ControllerContext controllerContext, string viewName, bool useCache)
+        {
+            return GetViewByConventionImpl(controllerContext, viewName, useCache, false, true);
+        }
+
+
+        ViewEngineResult GetViewByConventionImpl(ControllerContext controllerContext, string viewName, bool useCache, bool isPartial, bool runViewStartPages)
+        {
+            if (KnockoutRazorView.ViewExists(controllerContext.HttpContext, viewName, controllerContext.RouteData))
+            {
+                var view = new KnockoutRazorView(controllerContext, viewName, string.Empty, isPartial, runViewStartPages, new[] { "cshtml" });
+                return new ViewEngineResult(view, this);
+            }
+            return null;
+        }
+
+    }
+}
