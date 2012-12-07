6c023e4a22730a5d63f1fc1643121e36b0f4d451
Einar Ingebrigtsen <einar@dolittle.com>
2012-12-07 13:41:45 +0100

    Implemented an obersvable for dealing with query parameters - one way for now

---
+++ Source/Bifrost.JavaScript.Specs/navigation/for_observableQueryParameter/when_default_value_is_specified_and_query_parameter_does_not_exist.js
@@ -0,0 +1,24 @@
+﻿describe("when default value is specified but query parameter exist as well", function () {
+    var observable;
+
+    beforeEach(function () {
+        sinon.stub(History.Adapter, "bind", function (scope, event, callback) {
+            callback();
+        });
+        sinon.stub(History, "getState", function () {
+            return {
+                url: "http://www.somewhere.com/#?something=hello"
+            }
+        });
+        observable = ko.observableQueryParameter("something");
+    });
+
+    afterEach(function () {
+        History.Adapter.bind.restore();
+        History.getState.restore();
+    });
+
+    it("should hold the value from the query parameter", function () {
+        expect(observable()).toBe("hello");
+    });
+});
---
+++ Source/Bifrost.JavaScript/navigation/observableQueryParameter.js
@@ -0,0 +1,25 @@
+﻿if (typeof History !== "undefined" && typeof History.Adapter !== "undefined" && typeof ko !== "undefined") {
+    ko.observableQueryParameter = function (parameterName, defaultValue) {
+        var self = this;
+        var observable = null;
+
+        this.getState = function () {
+            var state = History.getState();
+            var uri = Bifrost.Uri.create(state.url);
+            if (uri.parameters.hasOwnProperty(parameterName)) {
+                return uri.parameters[parameterName];
+            }
+
+            return null;
+        }
+
+        History.Adapter.bind(window, "statechange", function () {
+            if (observable != null) {
+                observable(self.getState());
+            }
+        });
+        
+        observable = ko.observable(self.getState() || defaultValue);
+        return observable;
+    }
+}
