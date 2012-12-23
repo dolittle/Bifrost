c75ef5f5e376101b678edd19eaae30412fdabd5c
Einar Ingebrigtsen <einar@dolittle.com>
2012-12-23 12:31:23 +0100

    Started implementing a specific class map for events for MongoDb

---
+++ Source/Bifrost.MongoDb/EventClassMap.cs
@@ -0,0 +1,14 @@
+﻿using Bifrost.Events;
+using MongoDB.Bson.Serialization;
+
+namespace Bifrost.MongoDB
+{
+    public class EventClassMap : BsonClassMap<IEvent>
+    {
+        public EventClassMap()
+        {
+            AutoMap();
+            
+        }
+    }
+}
