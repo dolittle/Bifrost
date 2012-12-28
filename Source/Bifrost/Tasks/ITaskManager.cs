ff59784d4242c3dd8136f02f30a27d006f686583
Einar Ingebrigtsen <einar@dolittle.com>
2012-12-28 21:25:59 +0100

    Started working on a Task API for tasks that needs to run - maintenance tasks and similar

---
+++ Source/Bifrost/Tasks/ITaskManager.cs
@@ -0,0 +1,57 @@
+﻿#region License
+//
+// Copyright (c) 2008-2012, DoLittle Studios AS and Komplett ASA
+//
+// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
+// With one exception :
+//   Commercial libraries that is based partly or fully on Bifrost and is sold commercially, 
+//   must obtain a commercial license.
+//
+// You may not use file except in compliance with the License.
+// You may obtain a copy of the license at 
+//
+//   http://bifrost.codeplex.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Bifrost.Tasks
+{
+    /// <summary>
+    /// Defines a manager for working with <see cref="Task">Tasks</see>
+    /// </summary>
+    public interface ITaskManager
+    {
+        /// <summary>
+        /// Resume a task with given <see cref="TaskId"/>
+        /// </summary>
+        /// <typeparam name="T">Type of <see cref="Task"/> to resume</typeparam>
+        /// <param name="taskId">The <see cref="TaskId"/> that identifies the task</param>
+        /// <returns>The <see cref="Task"/>that is resumed</returns>
+        T Resume<T>(TaskId taskId) where T : Task;
+
+        /// <summary>
+        /// Start a given task
+        /// </summary>
+        /// <typeparam name="T">Type of <see cref="Task"/> to start</typeparam>
+        /// <param name="task">The <see cref="Task"/> to start</param>
+        void Start<T>(T task) where T : Task;
+
+        /// <summary>
+        /// Stop a task by its <see cref="TaskId"/>
+        /// </summary>
+        /// <param name="taskId"><see cref="TaskId"/> of the task to stop</param>
+        void Stop(TaskId taskId);
+
+        /// <summary>
+        /// Pause a task by its <see cref="TaskId"/>
+        /// </summary>
+        /// <param name="taskId"><see cref="TaskId"/> of the task to stop</param>
+        void Pause(TaskId taskId);
+    }
+}
---
+++ Source/Bifrost/Tasks/TaskId.cs
@@ -0,0 +1,57 @@
+﻿#region License
+//
+// Copyright (c) 2008-2012, DoLittle Studios AS and Komplett ASA
+//
+// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
+// With one exception :
+//   Commercial libraries that is based partly or fully on Bifrost and is sold commercially, 
+//   must obtain a commercial license.
+//
+// You may not use file except in compliance with the License.
+// You may obtain a copy of the license at 
+//
+//   http://bifrost.codeplex.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System;
+namespace Bifrost.Tasks
+{
+    /// <summary>
+    /// Defines an identifier for <see cref="Task">Tasks</see>
+    /// </summary>
+    public struct TaskId
+    {
+        /// <summary>
+        /// Gets or sets the actual value
+        /// </summary>
+        public Guid Value { get; set; }
+
+        /// <summary>
+        /// Implicitly convert from <see cref="TaskId"/> to <see cref="Guid"/>
+        /// </summary>
+        /// <param name="taskId"><see cref="TaskId"/> to convert from</param>
+        /// <returns>The <see cref="Guid"/> from the <see cref="TaskId"/></returns>
+        public static implicit operator Guid(TaskId taskId)
+        {
+            return taskId.Value;
+        }
+
+
+        /// <summary>
+        /// Implicitly convert from <see cref="Guid"/> to <see cref="TaskId"/>
+        /// </summary>
+        /// <param name="taskId"><see cref="Guid"/> to convert from</param>
+        /// <returns>The <see cref="TaskId"/> created from the <see cref="Guid"/> </returns>
+        public static implicit operator TaskId(Guid taskId)
+        {
+            return new TaskId { Value = taskId };
+        }
+    }
+}
