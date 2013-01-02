2b399afae56527f03c24a1ef74b5e1a2f38f2440
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-02 14:07:47 +0100

    Fixing regex for parsing functions - it now works with IE as expected..

--- Source/Bifrost.JavaScript/utils/functionParser.js
+++ Source/Bifrost.JavaScript/utils/functionParser.js
@@ -3,7 +3,8 @@ Bifrost.namespace("Bifrost", {
 		parse: function(func) {
 			var result = [];
 			
-			var arguments = func.toString ().match (/function\s+\w*\s*\((.*?)\)/)[1].split (/\s*,\s*/);
+            var match = func.toString ().match (/function\w*\s*\((.*?)\)/);
+			var arguments = match[1].split (/\s*,\s*/);
 			$.each(arguments, function(index, item) {
 				if( item.trim().length > 0 ) {
 					result.push({
