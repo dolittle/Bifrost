1ae0f89fd1cf000eb35dd2d8ec544c054df5602f
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-04 09:26:04 +0100

    Formalizing mapping of Concepts based on the convention of a concept being possible to create with default constructor and having a property called Value inside it

---
+++ Source/Bifrost.NHibernate/Concepts/MappingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Bifrost.NHibernate.Concepts
+{
+    public static class MappingExtensions
+    {
+        public static IdentityPart ConceptOf<T>(this IdentityPart identityPart)
+        {
+            identityPart.CustomType<ConceptValueType<T>>();
+            return identityPart;
+        }
+
+
+        public static PropertyPart ConceptOf<T>(this PropertyPart propertyPart)
+        {
+            propertyPart.CustomType<T>();
+            return propertyPart;
+        }
+    }
+}
