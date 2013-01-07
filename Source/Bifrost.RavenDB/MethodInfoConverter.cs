f106d836df88d46a793b69409c2244531c742b8a
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-07 15:12:49 +0100

    If type or method is not possible to resolve, return null - it could mean dodgy data or type does not exist anymore, either way, not something the RavenDB provider should take care of!

--- Source/Bifrost.RavenDB/MethodInfoConverter.cs
+++ Source/Bifrost.RavenDB/MethodInfoConverter.cs
@@ -15,30 +15,33 @@ namespace Bifrost.RavenDB
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            var jsonObject = JObject.Load(reader);
-            var className = jsonObject.Property("ClassName").Value.ToString();
-            var name = jsonObject.Property("Name").Value.ToString();
-            var assemblyName = jsonObject.Property("AssemblyName").Value.ToString();
-            var signature = jsonObject.Property("Signature").Value.ToString();
+            try
+            {
+                var jsonObject = JObject.Load(reader);
+                var className = jsonObject.Property("ClassName").Value.ToString();
+                var name = jsonObject.Property("Name").Value.ToString();
+                var assemblyName = jsonObject.Property("AssemblyName").Value.ToString();
+                var signature = jsonObject.Property("Signature").Value.ToString();
 
-            var start = signature.IndexOf('(') + 1;
-            var end = signature.IndexOf(')');
-            var parametersString = signature.Substring(start, end - start);
-            var parameterTypeStrings = parametersString.Split(',');
+                var start = signature.IndexOf('(') + 1;
+                var end = signature.IndexOf(')');
+                var parametersString = signature.Substring(start, end - start);
+                var parameterTypeStrings = parametersString.Split(',');
 
-            if (string.IsNullOrEmpty(className) ||
-                string.IsNullOrEmpty(name) ||
-                string.IsNullOrEmpty(assemblyName) ||
-                parameterTypeStrings == null)
-                throw new ArgumentException("Json object is not representing a MethodInfo");
-            
-            var typeName = string.Format("{0}, {1}", className, assemblyName);
-            var type = Type.GetType(typeName);
-            var method = type.GetMethods().Where(m => m.Name.Equals(name) && DoesSignatureMatch(m, parameterTypeStrings)).SingleOrDefault();
-            if (method == null)
-                throw new ArgumentException(string.Format("Not able to resolve method '{0}' on '{1}' during deserialization", name, typeName));
+                if (string.IsNullOrEmpty(className) ||
+                    string.IsNullOrEmpty(name) ||
+                    string.IsNullOrEmpty(assemblyName) ||
+                    parameterTypeStrings == null)
+                    throw new ArgumentException("Json object is not representing a MethodInfo");
 
-            return method;
+                var typeName = string.Format("{0}, {1}", className, assemblyName);
+                var type = Type.GetType(typeName);
+                var method = type.GetMethods().Where(m => m.Name.Equals(name) && DoesSignatureMatch(m, parameterTypeStrings)).SingleOrDefault();
+                return method;
+            }
+            catch {
+                return null;
+            }
         }
 
         bool DoesSignatureMatch(MethodInfo m, string[] parameterTypeStrings)
