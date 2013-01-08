1e34156abbf8ccd3d56612225f886f354ee05ac7
smithmx <smithmx@gmail.com>
2013-01-08 08:57:03 +0100

    Create a basic ConceptAs class for easily creating concepts that are basically a single atomic value.

--- Source/Bifrost.NHibernate/Concepts/MappingExtensions.cs
+++ Source/Bifrost.NHibernate/Concepts/MappingExtensions.cs
@@ -1,19 +1,24 @@
-﻿using FluentNHibernate.Mapping;
+﻿using System;
+using Bifrost.Concepts;
+using FluentNHibernate.Mapping;
 
 namespace Bifrost.NHibernate.Concepts
 {
     public static class MappingExtensions
     {
-        public static IdentityPart ConceptOf<T>(this IdentityPart identityPart)
+        public static IdentityPart ConceptOf<T, U>(this IdentityPart identityPart)
+            where U : IEquatable<U>
+            where T : ConceptAs<U>
         {
-            identityPart.CustomType<ConceptValueType<T>>();
+            identityPart.CustomType<ConceptValueType<T,U>>();
             return identityPart;
         }
 
-
-        public static PropertyPart ConceptOf<T>(this PropertyPart propertyPart)
+        public static PropertyPart ConceptOf<T, U>(this PropertyPart propertyPart)
+            where U : IEquatable<U>
+            where T : ConceptAs<U>
         {
-            propertyPart.CustomType<T>();
+            propertyPart.CustomType<ConceptValueType<T,U>>();
             return propertyPart;
         }
     }
