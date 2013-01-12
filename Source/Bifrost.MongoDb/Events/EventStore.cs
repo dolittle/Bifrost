d253261204c0874f0d0e99e4278f22b22a96691a
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-12 15:55:26 +0100

    Moving away from a typed collection

--- Source/Bifrost.MongoDb/Events/EventStore.cs
+++ Source/Bifrost.MongoDb/Events/EventStore.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using Bifrost.Events;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 
 namespace Bifrost.MongoDB.Events
 {
     public class EventStore : IEventStore
     {
         const string CollectionName = "Events";
+        const string EventType = "EventType";
+        const string Generation = "Generation";
+
         EventStoreConfiguration _configuration;
         MongoServer _server;
         MongoDatabase _database;
-        MongoCollection<IEvent> _collection;
+        //MongoCollection<IEvent> _collection;
+
+        MongoCollection _collection;
         IEventMigrationHierarchyManager _eventMigrationHierarchyManager;
 
         public EventStore(EventStoreConfiguration configuration, IEventMigrationHierarchyManager eventMigrationHierarchyManager)
@@ -29,14 +37,16 @@ namespace Bifrost.MongoDB.Events
             if (!_database.CollectionExists(CollectionName))
                 _database.CreateCollection(CollectionName);
 
-            _collection = _database.GetCollection<IEvent>(CollectionName);
+            _collection = _database.GetCollection(CollectionName);
+            //_collection = _database.GetCollection<IEvent>(CollectionName);
+            //BsonSerializer.RegisterSerializer(typeof(IEvent), new EventSerializer());
         }
 
         public CommittedEventStream GetForEventSource(EventSource eventSource, Guid eventSourceId)
         {
             var eventSourceType = eventSource.GetType();
 
-            var events = _collection.FindAll().AsQueryable()
+            var events = _collection.FindAllAs<IEvent>().AsQueryable()
                                 .Where(
                                     e => e.EventSourceId == eventSourceId &&
                                             e.EventSource == eventSourceType.AssemblyQualifiedName
@@ -52,8 +62,15 @@ namespace Bifrost.MongoDB.Events
             var eventArray = uncommittedEventStream.ToArray();
             for (var eventIndex = 0; eventIndex < eventArray.Length; eventIndex++)
             {
+                var logicalEventType = _eventMigrationHierarchyManager.GetLogicalTypeForEvent(eventType);
+                var migrationLevel = _eventMigrationHierarchyManager.GetCurrentMigrationLevelForLogicalEvent(logicalEventType);
+
                 var @event = eventArray[eventIndex];
-                _collection.Insert(@event);
+                var eventType = @event.GetType();
+                var eventDocument = @event.ToBsonDocument();
+                eventDocument[EventType] = string.Format("{0}, {1}", eventType.FullName, eventType.Assembly.GetName().Name);
+                eventDocument[Generation] = migrationLevel;
+                _collection.Insert(eventDocument);
             }
 
             var committedEventStream = new CommittedEventStream(uncommittedEventStream.EventSourceId);
@@ -63,7 +80,14 @@ namespace Bifrost.MongoDB.Events
 
         public EventSourceVersion GetLastCommittedVersion(EventSource eventSource, Guid eventSourceId)
         {
-            var @event = _collection.FindAll().AsQueryable()
+            return EventSourceVersion.Zero;
+
+            var query = Query.EQ("EventSourceId", eventSourceId);
+
+            var cursor = _collection.FindAs<BsonDocument>(query);
+            
+
+            var @event = _collection.FindAllAs<IEvent>().AsQueryable()
                             .Where(e => e.EventSourceId == eventSourceId)
                                 .OrderByDescending(e => e.Version)
                             .FirstOrDefault();
@@ -76,7 +100,7 @@ namespace Bifrost.MongoDB.Events
 
         public IEnumerable<IEvent> GetBatch(int batchesToSkip, int batchSize)
         {
-            var events = _collection.FindAll().AsQueryable().Skip(batchSize * batchesToSkip).Take(batchSize);
+            var events = _collection.FindAllAs<IEvent>().AsQueryable().Skip(batchSize * batchesToSkip).Take(batchSize);
             return events.ToArray();
         }
     }
