00967d5e25bb55e4144f828a9ae970f311df8177
smithmx@gmail.com <smithmx@gmail.com>
2013-01-13 17:26:15 +0100

    Make it easier to see if a CommandScenario was successful or not

---
+++ Source/Bifrost.MSpec/Extensions/CommandScenarioExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Bifrost.Commands;
+using Bifrost.Testing;
+using Machine.Specifications;
+
+namespace Bifrost.MSpec.Extensions
+{
+    public static class CommandScenarioExtensions
+    {
+        public static void ShouldBeASuccessfulScenario<T>(this CommandScenario<T> scenario) where T : class, ICommand
+         {
+             scenario.IsSuccessful().ShouldBeTrue();
+         }
+
+        public static void ShouldBeAnUnsuccessfulScenario<T>(this CommandScenario<T> scenario) where T : class, ICommand
+         {
+             scenario.IsUnsuccessful().ShouldBeTrue();
+         }
+    }
+
+}
---
+++ Source/Bifrost.Testing/Exceptions/CommandScenarioNotRunException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace Bifrost.Testing.Exceptions
+{
+    public class CommandScenarioNotRunException : Exception
+    {
+        public CommandScenarioNotRunException()
+        {
+        }
+
+        public CommandScenarioNotRunException(string message)
+            : base(message)
+        {
+        }
+
+        public CommandScenarioNotRunException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected CommandScenarioNotRunException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
