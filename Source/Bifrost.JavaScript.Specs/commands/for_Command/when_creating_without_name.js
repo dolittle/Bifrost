eb8da6e7bc9511a488dfee7fbcc0d712c3f62882
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-16 13:58:33 +0100

    Added spec to command

---
+++ Source/Bifrost.JavaScript.Specs/commands/for_Command/when_creating_without_name.js
@@ -0,0 +1,16 @@
+﻿describe("when creating without name", function () {
+    var parameters = {
+        commandCoordinator: {
+        },
+        commandValidationService: {
+            applyRulesTo: sinon.stub()
+        },
+        options: {
+        }
+    }
+    var command = Bifrost.commands.Command.create(parameters);
+
+    it("should not get validation rules", function () {
+        expect(parameters.commandValidationService.applyRulesTo.called).toBe(false);
+    });
+});
