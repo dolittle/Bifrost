16cc811d9ab8d37de404eaae4534cf4a2b6cb7d7
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-23 10:29:53 +0100

    Fixing the ordering of checks - skip _type and _namespace earlier, plus check if has prototype before checking wether or not it is a Bifrost.Type

--- Source/Bifrost.JavaScript/commands/commandValidationService.js
+++ Source/Bifrost.JavaScript/commands/commandValidationService.js
@@ -7,9 +7,9 @@
             if (!target.hasOwnProperty(property)) return true;
             if (ko.isObservable(target[property])) return false;
             if (typeof target[property] === "function") return true;
-            if (target[property] instanceof Bifrost.Type) return true;
             if (property == "_type") return true;
             if (property == "_namespace") return true;
+            if ((target[property].prototype != null) && (target[property] instanceof Bifrost.Type)) return true;
 
             return false;
         }
