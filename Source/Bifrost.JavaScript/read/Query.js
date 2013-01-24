4da6a6e525fa535afeef5b24dc1911f995e1dc2f
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-24 11:26:17 +0100

    Proxy Generation and client side representation started for QueryFor

---
+++ Source/Bifrost.JavaScript/read/Query.js
@@ -0,0 +1,11 @@
+﻿Bifrost.namespace("Bifrost.read", {
+    Query: Bifrost.Type.extend(function () {
+        var self = this;
+        this.name = "";
+        var allObservable = ko.observableArray();
+
+        this.all = function () {
+            return allObservable;
+        }
+    })
+});
