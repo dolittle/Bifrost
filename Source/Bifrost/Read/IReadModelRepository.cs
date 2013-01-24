0d71f1b8029ec6843b0dea5f0f1c92cf7d04bbd4
Einar Ingebrigtsen <einar@dolittle.com>
2013-01-24 08:51:22 +0100

    Started fleshing out the Read side - eventually the IView and what is called View side today will disappear in favor of Read!

---
+++ Source/Bifrost/Read/IReadModelRepository.cs
@@ -0,0 +1,62 @@
+﻿#region License
+//
+// Copyright (c) 2008-2012, DoLittle Studios AS and Komplett ASA
+//
+// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
+// With one exception :
+//   Commercial libraries that is based partly or fully on Bifrost and is sold commercially, 
+//   must obtain a commercial license.
+//
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the license at 
+//
+//   http://bifrost.codeplex.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System.Linq;
+namespace Bifrost.Read
+{
+    /// <summary>
+    /// Defines a repository for dealing with ReadModels
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IReadModelRepositoryFor<T>
+    {
+        /// <summary>
+        /// Gets a queryable to use for querying
+        /// </summary>
+        IQueryable<T> Query { get; }
+
+        /// <summary>
+        /// Insert a newly created ReadModel
+        /// </summary>
+        /// <param name="readModel">ReadModel to insert</param>
+        void Insert(T readModel);
+
+        /// <summary>
+        /// Update an existing ReadModel
+        /// </summary>
+        /// <param name="readModel">ReadModel to update</param>
+        void Update(T readModel);
+
+        /// <summary>
+        /// Delete an existing ReadModel
+        /// </summary>
+        /// <param name="readModel">ReadModel to delete</param>
+        void Delete(T readModel);
+
+        /// <summary>
+        /// Get a ReadModel by its id
+        /// </summary>
+        /// <param name="id">Id to lookup</param>
+        /// <returns></returns>
+        T GetById(object id);
+    }
+}
---
+++ Source/Bifrost/Read/ReadModelRepositoryFor.cs
@@ -0,0 +1,68 @@
+﻿#region License
+//
+// Copyright (c) 2008-2012, DoLittle Studios AS and Komplett ASA
+//
+// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
+// With one exception :
+//   Commercial libraries that is based partly or fully on Bifrost and is sold commercially, 
+//   must obtain a commercial license.
+//
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the license at 
+//
+//   http://bifrost.codeplex.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+using System.Linq;
+using Bifrost.Entities;
+
+namespace Bifrost.Read
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IReadModelRepositoryFor{T}"/>
+    /// </summary>
+    /// <typeparam name="T">Type of ReadModel the repository represents</typeparam>
+    public class ReadModelRepositoryFor<T> : IReadModelRepositoryFor<T>
+    {
+        IEntityContext<T> _entityContext;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ReadModelRepositoryFor{T}"/>
+        /// </summary>
+        /// <param name="entityContext"><see cref="IEntityContext{T}"/> used by the repository</param>
+        public ReadModelRepositoryFor(IEntityContext<T> entityContext)
+        {
+            _entityContext = entityContext;
+        }
+
+#pragma warning disable 1591
+        public IQueryable<T> Query { get { return _entityContext.Entities; } }
+
+        public void Insert(T readModel)
+        {
+            _entityContext.Insert(readModel);
+        }
+
+        public void Update(T readModel)
+        {
+            _entityContext.Update(readModel);
+        }
+
+        public void Delete(T readModel)
+        {
+            _entityContext.Delete(readModel);
+        }
+
+        public T GetById(object id)
+        {
+            return _entityContext.GetById(id);
+        }
+#pragma warning restore 1591
+    }
+}
