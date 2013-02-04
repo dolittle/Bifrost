ca2ccf3865913acbac78712801671028a06c985d
smithmx@gmail.com <smithmx@gmail.com>
2013-02-04 20:46:02 +0100

    Failing specs for Serialization.  Converters work if the Concept is contained within another class but not if the concept is the only thing being serialized / deserialized.

---
+++ Source/Bifrost.JSON.Specs/Serialization/for_Serializer/when_serializing_a_concept.cs
@@ -0,0 +1,23 @@
+using System;
+using Bifrost.JSON.Serialization;
+using Bifrost.Testing.Fakes.Concepts;
+using Machine.Specifications;
+
+namespace Bifrost.JSON.Specs.Serialization.for_Serializer
+{
+    [Subject(typeof(Serializer))]
+    public class when_serializing_a_concept : given.a_serializer
+    {
+        static ConceptAsGuid to_serialize;
+        static string serialized_version;
+
+        Establish context = () =>
+                                {
+                                    to_serialize = Guid.NewGuid();
+                                };
+
+        Because of = () => serialized_version = serializer.ToJson(to_serialize);
+
+        It should_contain_the_guid_value = () => serialized_version.ShouldEqual(to_serialize.Value.ToString());
+    }
+}
