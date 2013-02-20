ca0e265922b73e2178f7a68284ee16fcff03dc86
Pavneet Singh Saund <pavneet@dolittle.com>
2013-02-20 22:30:42 +0100

    Seems like #164 is fixed already. putting the exception back in

--- Source/Bifrost.JavaScript/commands/commandValidationService.js
+++ Source/Bifrost.JavaScript/commands/commandValidationService.js
@@ -100,8 +100,7 @@
                         if (step in member) {
                             member = member[step];
                         } else {
-//                            throw "Error applying validation rules: " + step + " is not a member of " + member + " (" + rule + ")";
-                            console.log( "Error applying validation rules: " + step + " is not a member of " + member + " (" + rule + ")");
+                            throw "Error applying validation rules: " + step + " is not a member of " + member + " (" + rule + ")";
                         }
                     }
 
