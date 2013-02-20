527bdaaa02d8098bc0446b2a94605e4e09de5b6f
Einar Ingebrigtsen <einar@dolittle.com>
2013-02-20 13:15:20 -0800

    Added parameterized version for all nuspecs

--- Source/PushToNuget.bat
+++ Source/PushToNuget.bat
@@ -1,13 +1,17 @@
-nuget pack Bifrost\Bifrost.nuspec -Symbols
-nuget pack Bifrost.JSON\Bifrost.JSON.nuspec -Symbols
-nuget pack Bifrost.Ninject\Bifrost.Ninject.nuspec -Symbols
-nuget pack Bifrost.Web\Bifrost.Web.nuspec -Symbols
-nuget pack Bifrost.SignalR\Bifrost.SignalR.nuspec -Symbols
-nuget pack Bifrost.RavenDB\Bifrost.RavenDB.nuspec -Symbols
-nuget pack Bifrost.RavenDB.Embedded\Bifrost.RavenDB.Embedded.nuspec -Symbols
-nuget pack Bifrost.MongoDB\Bifrost.MongoDB.nuspec -Symbols
-nuget pack Bifrost.NHibernate\Bifrost.NHibernate.nuspec -Symbols
-nuget pack Bifrost.Mimir.Web\Bifrost.Mimir.nuspec -Symbols
+del *.nupkg /s /y
+
+set version=1.0.0.1
+
+nuget pack Bifrost\Bifrost.nuspec -Symbols -version %version%
+nuget pack Bifrost.JSON\Bifrost.JSON.nuspec -Symbols -version %version%
+nuget pack Bifrost.Ninject\Bifrost.Ninject.nuspec -Symbols -version %version%
+nuget pack Bifrost.JavaScript\Bifrost.JS.nuspec -version %version%
+nuget pack Bifrost.Web\Bifrost.Web.nuspec -Symbols -version %version%
+nuget pack Bifrost.SignalR\Bifrost.SignalR.nuspec -Symbols -Properties version=%version%
+nuget pack Bifrost.RavenDB\Bifrost.RavenDB.nuspec -Symbols -version %version%
+nuget pack Bifrost.RavenDB.Embedded\Bifrost.RavenDB.Embedded.nuspec -Symbols -version %version%
+nuget pack Bifrost.MongoDB\Bifrost.MongoDB.nuspec -Symbols -version %version%
+nuget pack Bifrost.NHibernate\Bifrost.NHibernate.nuspec -Symbols -version %version%
+nuget pack Bifrost.Mimir.Web\Bifrost.Mimir.nuspec -Symbols -Properties version=%version%
 
 for %%f in (*.nupkg) do nuget push %%f
-
