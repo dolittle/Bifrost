e8a2f77ebc8bf74df075fe441d6036e61b44ab8e
markwoodhall <mark.woodhall@gmail.com>
2013-03-02 20:24:21 +0000

    #147 Add a null implementation of statistics store

---
+++ Source/Bifrost/Statistics/NullStatisticsStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bifrost.Statistics
+{
+    /// <summary>
+    /// Represents a null implementation of <see cref="IStatisticsStore" />
+    /// </summary>
+    public class NullStatisticsStore : IStatisticsStore
+    {
+#pragma warning disable 1591 // Xml Comments
+        public void Add(IStatistic statistic)
+        {
+        }
+#pragma warning restore 1591 // Xml Comments
+    }
+}
