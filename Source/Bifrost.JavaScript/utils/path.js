0dd78edcee84c0660e9395e1262c50d1e5a61307
Einar Ingebrigtsen <einar@dolittle.com>
2013-03-19 15:35:50 +0100

    Adding more functionality for dealing with paths

--- Source/Bifrost.JavaScript/utils/path.js
+++ Source/Bifrost.JavaScript/utils/path.js
@@ -1,8 +1,21 @@
 ﻿Bifrost.namespace("Bifrost", {
     path: {
+        makeRelative: function (fullPath) {
+            if (fullPath.indexOf("/") == 0) return fullPath.substr(1);
+
+            return fullPath;
+        },
         getPathWithoutFilename: function (fullPath) {
             var lastIndex = fullPath.lastIndexOf("/");
             return fullPath.substr(0, lastIndex);
+        },
+        getFilename: function (fullPath) {
+            var lastIndex = fullPath.lastIndexOf("/");
+            return fullPath.substr(lastIndex+1);
+        },
+        changeExtension: function (fullPath, newExtension) {
+            var lastIndex = fullPath.lastIndexOf(".");
+            return fullPath.substr(0, lastIndex) + "." + newExtension;
         }
     }
 });
