b8c036691ff31d9af544df108a37991b3ff27714
Einar Ingebrigtsen <einar@dolittle.com>
2013-03-30 00:50:42 +0100

    Added loading of view and viewModel in parallel - better load performance, plus things are ready when they should be!

--- Source/Bifrost.JavaScript/views/viewLoader.js
+++ Source/Bifrost.JavaScript/views/viewLoader.js
@@ -1,10 +1,23 @@
 ﻿Bifrost.namespace("Bifrost.views", {
-    viewLoader: Bifrost.Singleton(function () {
+    viewLoader: Bifrost.Singleton(function (viewModelManager) {
         var self = this;
+
+        this.viewModelManager = viewModelManager;
         
         this.load = function (path) {
             var promise = Bifrost.execution.Promise.create();
-            require(["text!" + path + "!strip"], function (view) {
+
+            if (!path.startsWith("/")) path = "/" + path;
+
+            var files = [];
+            files.push("text!" + path + "!strip");
+
+            if (self.viewModelManager.hasForView(path)) {
+                var viewModelFile = Bifrost.path.changeExtension(path, "js");
+                files.push(viewModelFile);
+            }
+
+            require(files, function (view) {
                 promise.signal(view);
             });
             return promise;
