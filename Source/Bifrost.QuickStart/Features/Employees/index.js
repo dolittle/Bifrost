0b922cfae53032845450dd63a06a40ee0e52efce
Einar Ingebrigtsen <einar@dolittle.com>
2013-04-11 09:19:36 +0200

    Removing not used things from the Quickstart

--- Source/Bifrost.QuickStart/Features/Employees/index.js
+++ Source/Bifrost.QuickStart/Features/Employees/index.js
@@ -1,7 +1,5 @@
 ﻿Bifrost.namespace("Bifrost.QuickStart.Features.Employees", {
     index: Bifrost.views.ViewModel.extend(function () {
         var self = this;
-
-        this.value = ko.observable(42);
     })
 });
