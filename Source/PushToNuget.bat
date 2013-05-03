afdb17d2a77243bc1e40a940313ee3dee9d052e8
Pavneet Singh Saund <pav.saund@gmail.com>
2013-05-03 22:06:19 +0200

    Making sure that symbol files are generated correctly
    Removed direct file references in nuspec files. Pointing at .csproj files for packing

--- Source/PushToNuget.bat
+++ Source/PushToNuget.bat
@@ -2,17 +2,17 @@ del *.nupkg /f /q
 set nuget=solutions\.nuget\nuget.exe
 set version=1.0.0.7
 
-%nuget% pack Bifrost\Bifrost.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.JSON\Bifrost.JSON.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.Ninject\Bifrost.Ninject.nuspec -Symbols -version %version%
+%nuget% pack Bifrost\Bifrost.csproj -Symbols -version %version%
+%nuget% pack Bifrost.JSON\Bifrost.JSON.csproj -Symbols -version %version%
+%nuget% pack Bifrost.Ninject\Bifrost.Ninject.csproj -Symbols -version %version%
 %nuget% pack Bifrost.JavaScript\Bifrost.JS.nuspec -version %version%
-%nuget% pack Bifrost.Web\Bifrost.Web.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.SignalR\Bifrost.SignalR.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.RavenDB\Bifrost.RavenDB.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.RavenDB.Embedded\Bifrost.RavenDB.Embedded.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.MongoDB\Bifrost.MongoDB.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.NHibernate\Bifrost.NHibernate.nuspec -Symbols -version %version%
-%nuget% pack Bifrost.Mimir.Web\Bifrost.Mimir.nuspec -Symbols -version %version%
+%nuget% pack Bifrost.Web\Bifrost.Web.csproj -Symbols -version %version%
+%nuget% pack Bifrost.SignalR\Bifrost.SignalR.csproj -Symbols -version %version%
+%nuget% pack Bifrost.RavenDB\Bifrost.RavenDB.csproj -Symbols -version %version%
+%nuget% pack Bifrost.RavenDB.Embedded\Bifrost.RavenDB.Embedded.csproj -Symbols -version %version%
+%nuget% pack Bifrost.MongoDB\Bifrost.MongoDB.csproj -Symbols -version %version%
+%nuget% pack Bifrost.NHibernate\Bifrost.NHibernate.csproj -Symbols -version %version%
+%nuget% pack Bifrost.Mimir.Web\Bifrost.Mimir.Web.csproj -Symbols -version %version%
 %nuget% pack Bifrost.Default\Bifrost.Default.nuspec -version %version%
 %nuget% pack Bifrost.QuickStart\Bifrost.QuickStart.nuspec -version %version%
 
