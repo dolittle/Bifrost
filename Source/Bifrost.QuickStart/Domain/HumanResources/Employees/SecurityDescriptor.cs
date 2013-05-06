de8adee758cc59228fc1bd3659b7992123dfa901
Einar Ingebrigtsen <einar@dolittle.com>
2013-05-06 07:15:45 +0200

    Fixing project - make things compile!

---
+++ Source/Bifrost.QuickStart/Domain/HumanResources/Employees/SecurityDescriptor.cs
@@ -0,0 +1,13 @@
+﻿using Bifrost.Commands;
+using Bifrost.Security;
+
+namespace Bifrost.QuickStart.Domain.HumanResources.Employees
+{
+    public class SecurityDescriptor : BaseSecurityDescriptor
+    {
+        public SecurityDescriptor()
+        {
+            When.Handling().Commands().InstanceOf<RegisterEmployee>(s => s.User().MustBeInRole("Create"));
+        }
+    }
+}
