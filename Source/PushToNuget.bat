ef5a0f8991ea0fb48863993432da1327760b5487
Pavneet Singh Saund <pav.saund@gmail.com>
2013-05-07 23:37:00 +0200

    uncommented the push nuget packages command

--- Source/PushToNuget.bat
+++ Source/PushToNuget.bat
@@ -3,6 +3,4 @@ del *.nupkg /f /q
 call CreateBifrostPackages.bat
 call CreateExamplePackages.bat 1.0.0.7
 
-
-
-@ECHO for %%f in (*.nupkg) do %nuget% push %%f
+for %%f in (*.nupkg) do %nuget% push %%f
