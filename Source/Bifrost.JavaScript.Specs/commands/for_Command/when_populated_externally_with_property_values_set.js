764813df93f1ebf8abd48149cef8e801f137302b
Einar Ingebrigtsen <einar@dolittle.com>
2013-06-04 23:54:02 +0200

    Adding more specs

---
+++ Source/Bifrost.JavaScript.Specs/commands/for_Command/when_populated_externally_with_property_values_set.js
@@ -0,0 +1,37 @@
+﻿describe("when populated externally with property values set", function () {
+    var parameters = {
+        commandCoordinator: {
+        },
+        commandValidationService: {
+            applyRulesTo: function (command) {
+            }
+        },
+        commandSecurityService: {
+            getContextFor: function () {
+                return {
+                    continueWith: function () { }
+                };
+            }
+        },
+        options: {
+        }
+    }
+
+    var commandType = Bifrost.commands.Command.extend(function () {
+        this.someValue = ko.observable(42);
+        this.someArray = ko.observableArray();
+    });
+
+    var newValues = {
+        someValue: 43,
+        someArray: [1, 2, 3]
+    };
+
+    var command = commandType.create(parameters);
+    command.populatedExternally();
+    command.setPropertyValuesFrom(newValues);
+
+    it("should be considered ready", function () {
+        expect(command.isReady()).toBe(true);
+    });
+});
---
+++ Source/Bifrost.JavaScript.Specs/commands/for_Command/when_populated_externally_without_property_values_set.js
@@ -0,0 +1,29 @@
+﻿describe("when populated externally without property values set", function () {
+
+    var securityContext = "SecurityContext";
+    var command = Bifrost.commands.Command.create({
+        commandCoordinator: {},
+        commandValidationService: {
+            applyRulesTo: function (command) {
+                commandAppliedTo = command
+            }
+        },
+        commandSecurityService: {
+            getContextFor: function (command) {
+                commandAskedForSecurityContext = command;
+                return {
+                    continueWith: function (callback) {
+                        callback(securityContext);
+                    }
+                }
+            }
+        },
+        options: {}
+    });
+
+    command.populatedExternally();
+
+    it("should not be considered ready", function () {
+        expect(command.isReady()).toBe(false);
+    });
+});
