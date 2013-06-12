39dacc43c3a56a7aebd8fc6fd3606924de61e021
Einar Ingebrigtsen <einar@dolittle.com>
2013-06-12 13:27:33 +0200

    Adding specs for making sure hasChanges is acting as expected!

---
+++ Source/Bifrost.JavaScript.Specs/commands/for_hasChanges/when_value_changes_to_value_not_same_as_initial_value_then_back_again.js
@@ -0,0 +1,22 @@
+﻿describe("when value changes to value not same as initial value then back again", function () {
+    var changeCount = 0;
+
+    var target = ko.observable(43);
+    ko.extenders.hasChanges(target, {});
+
+    target.hasChanges.subscribe(function () {
+        changeCount++;
+    });
+    target.setInitialValue(43);
+
+    target(42);
+    target(43);
+
+    it("should not have changes", function () {
+        expect(target.hasChanges()).toBe(false);
+    });
+
+    it("should change hasChanges twice", function () {
+        expect(changeCount).toBe(2);
+    });
+});
