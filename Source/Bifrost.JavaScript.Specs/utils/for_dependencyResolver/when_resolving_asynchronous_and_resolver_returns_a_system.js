900444da4ee8b69dd50a31ee106674c9172e3632
Einar Ingebrigtsen <einar@dolittle.com>
2013-06-18 21:49:09 +0200

    Fixing a few specs - making them run more independent from each other!

--- Source/Bifrost.JavaScript.Specs/utils/for_dependencyResolver/when_resolving_asynchronous_and_resolver_returns_a_system.js
+++ Source/Bifrost.JavaScript.Specs/utils/for_dependencyResolver/when_resolving_asynchronous_and_resolver_returns_a_system.js
@@ -1,27 +1,30 @@
 describe("when resolving asynchronous and resolver returns a system", function() {
-	var ns = {};
-	Bifrost.dependencyResolvers = {
-		getAll: function() {
-			return [{
-				canResolve: function() {
-					return true;
-				},
-				resolve: function() {
-					return "The result";
-				}
-			}];
-		}
-	};
+    var ns = {};
+    var result = null;
+    beforeEach(function () {
+        Bifrost.dependencyResolvers = {
+            getAll: function () {
+                return [{
+                    canResolve: function () {
+                        return true;
+                    },
+                    resolve: function () {
+                        return "The result";
+                    }
+                }];
+            }
+        };
 
-    Bifrost.configure.reset();
-    Bifrost.configure.onReady();
+        Bifrost.configure.reset();
+        Bifrost.configure.onReady();
 
-	var result = null;
-	Bifrost.dependencyResolver
-		.beginResolve(ns, "something")
-		.continueWith(function(parameter, nextPromise) {
-			result = parameter;
-		});
+        
+        Bifrost.dependencyResolver
+            .beginResolve(ns, "something")
+            .continueWith(function (parameter, nextPromise) {
+                result = parameter;
+            });
+    });
 
 	it("should continue with system from resolver as parameter", function() {
 		expect(result).toBe("The result");
