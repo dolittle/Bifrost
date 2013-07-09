f117b854e78278c12d3d7ec63f5b8de82af46f19
smithmx@gmail.com <smithmx@gmail.com>
2013-07-09 10:48:43 +0200

    Add TypeExtension to check for IsModelRuleProperty

---
+++ Source/Bifrost/Validation/TypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bifrost.Extensions;
+
+namespace Bifrost.Validation
+{
+    /// <summary>
+    /// Contains Type extension specific to validation
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Check if a type is a ModelRuleProperty{T}
+        /// </summary>
+        /// <returns>True if this type is a ModelRuleProperty, otherwise false</returns>
+        public static bool IsModelRuleProperty(this Type type)
+        {
+            return type.IsDerivedFromOpenGeneric(typeof (ModelRule<>));
+        }
+    }
+}
