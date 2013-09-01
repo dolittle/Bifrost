41eb66015f1189e021c5bd8417e075767af3b6f7
smithmx@gmail.com <smithmx@gmail.com>
2013-09-01 22:23:50 +0200

    We might discover more than one validator for a type, so created a composite validator to allow us to combine them... not sure this is a good idea but we can see...

---
+++ Source/Bifrost.Silverlight/Validation/CompositeValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Bifrost.Validation
+{
+    /// <summary>
+    /// Combines multiples validators into a single validator
+    /// </summary>
+    /// <typeparam name="T">Type that the composite validator validates</typeparam>
+    public class CompositeValidator<T> : AbstractValidator<T>
+    {
+        List<IValidator<T>> registeredValidators = new List<IValidator<T>>();
+
+        /// <summary>
+        /// Instantiates an instance of a <see cref="CompositeValidator{T}"/>
+        /// </summary>
+        /// <param name="validators"></param>
+        public CompositeValidator(IEnumerable<IValidator<T>> validators)
+        {
+            registeredValidators.AddRange(validators);
+        }
+
+        /// <summary>
+        /// Validates the ValidationContext
+        /// </summary>
+        /// <param name="context">Context of the validation</param>
+        /// <returns>ValidationResult</returns>
+        public override ValidationResult Validate(ValidationContext<T> context)
+        {
+            var errors = registeredValidators.SelectMany(x => x.Validate(context).Errors);
+            return new ValidationResult(errors);
+        }
+    }
+}
