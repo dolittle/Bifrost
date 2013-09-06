5c0771780fab36efa4cc0e324ebb0bfcbd8b7cdc
Einar Ingebrigtsen <einar@dolittle.com>
2013-09-06 08:26:55 +0200

    Added spec for Queryable for when executing

---
+++ Source/Bifrost.JavaScript.Specs/read/for_Queryable/when_executing.js
@@ -0,0 +1,51 @@
+﻿describe("when executing", function () {
+        var query = {
+            areAllParametersSet: function () {
+                return true;
+            }
+        };
+        var observable = ko.observableArray();
+        var queryService = null;
+
+        var pagingInfoType = null;
+        
+        var returnValue = null;
+        var queryable = null;
+
+        beforeEach(function () {
+            pagingInfoType = Bifrost.read.PagingInfo;
+            Bifrost.read.PagingInfo = {
+                create: function () {
+                    return {};
+                }
+            };
+
+            queryService = {
+                execute: function () {
+                    return {
+                        continueWith: function (callback) {
+                        }
+                    }
+                }
+            };
+
+            queryable = Bifrost.read.Queryable.new({
+                query: query,
+                queryService: queryService,
+                targetObservable: observable
+            });
+
+            returnValue = queryable.execute();
+
+        });
+
+        afterEach(function () {
+            Bifrost.read.PagingInfo = pagingInfoType;
+        });
+
+
+        it("should return itself", function () {
+            expect(returnValue).toBe(queryable);
+        });
+
+    });
