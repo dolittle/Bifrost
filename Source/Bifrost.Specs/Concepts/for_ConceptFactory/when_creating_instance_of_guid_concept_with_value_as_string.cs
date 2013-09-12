5a3c4ee5bf865551616ecca1131029257552b80c
Einar Ingebrigtsen <einar@dolittle.com>
2013-09-12 08:24:43 +0200

    Fixed ConceptFactory to be able to convert any value coming in with the wrong type to the correct type for the concept - added specs for it as well! See #395

---
+++ Source/Bifrost.Specs/Concepts/for_ConceptFactory/when_creating_instance_of_guid_concept_with_value_as_string.cs
@@ -0,0 +1,16 @@
+﻿using Bifrost.Concepts;
+using Machine.Specifications;
+
+namespace Bifrost.Specs.Concepts.for_ConceptFactory
+{
+    public class when_creating_instance_of_guid_concept_with_value_as_string
+    {
+        const   string  guid_value_as_string = "4AB92720-3138-4A7B-A7E9-2A49F6624736";
+
+        static GuidConcept result;
+
+        Because of = () => result = ConceptFactory.CreateConceptInstance(typeof(GuidConcept), guid_value_as_string) as GuidConcept;
+
+        It should_hold_the_correct_guid_value = () => result.Value.ToString().ToUpper().ShouldEqual(guid_value_as_string);
+    }
+}
