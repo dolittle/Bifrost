5d38bdb6dbc3cd75d0b99bb47d93e9f677db8864
Einar Ingebrigtsen <einar@dolittle.com>
2013-11-09 20:23:48 +0100

    Adding a naive solution for including security information on Commands - this will ofcourse need to improve over time!!!

--- Source/Bifrost.JavaScript/commands/Command.js
+++ Source/Bifrost.JavaScript/commands/Command.js
@@ -279,16 +279,19 @@ Bifrost.namespace("Bifrost.commands", {
                 if (Bifrost.isArray(validators) && validators.length > 0) self.validators(validators);
                 commandValidationService.validateSilently(this);
             }
-            commandSecurityService.getContextFor(lastDescendant).continueWith(function (securityContext) {
-                lastDescendant.securityContext(securityContext);
+            
+            if (lastDescendant.securityContext() == null) {
+                commandSecurityService.getContextFor(lastDescendant).continueWith(function (securityContext) {
+                    lastDescendant.securityContext(securityContext);
 
-                if (ko.isObservable(securityContext.isAuthorized)) {
-                    lastDescendant.isAuthorized(securityContext.isAuthorized());
-                    securityContext.isAuthorized.subscribe(function (newValue) {
-                        lastDescendant.isAuthorized(newValue);
-                    });
-                }
-            });
+                    if (ko.isObservable(securityContext.isAuthorized)) {
+                        lastDescendant.isAuthorized(securityContext.isAuthorized());
+                        securityContext.isAuthorized.subscribe(function (newValue) {
+                            lastDescendant.isAuthorized(newValue);
+                        });
+                    }
+                });
+            }
         };
     })
 });
