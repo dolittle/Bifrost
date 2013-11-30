15e6f1235fb7f922dfa5a113e8d609fe752489dd
smithmx@gmail.com <smithmx@gmail.com>
2013-11-30 18:34:55 +0100

    Handle null values for string and datetime in the ConceptFactory

---
+++ Source/Bifrost.Specs/Concepts/for_ConceptFactory/when_creating_instance_of_guid_concept_with_value_as_null.cs
@@ -0,0 +1,15 @@
+﻿using Bifrost.Concepts;
+using Machine.Specifications;
+
+namespace Bifrost.Specs.Concepts.for_ConceptFactory
+{
+    [Subject(typeof(ConceptFactory))]
+    public class when_creating_instance_of_guid_concept_with_value_as_null
+    {
+        static GuidConcept result;
+
+        Because of = () => result = ConceptFactory.CreateConceptInstance(typeof(GuidConcept), null) as GuidConcept;
+
+        It should_be_empty = () => result.IsEmpty().ShouldBeTrue();
+    }
+}
--- Source/Bifrost.Specs/Concepts/for_ConceptFactory/when_creating_instance_of_guid_concept_with_value_as_string.cs
+++ Source/Bifrost.Specs/Concepts/for_ConceptFactory/when_creating_instance_of_guid_concept_with_value_as_string.cs
@@ -3,6 +3,7 @@ using Machine.Specifications;
 
 namespace Bifrost.Specs.Concepts.for_ConceptFactory
 {
+    [Subject(typeof(ConceptFactory))]
     public class when_creating_instance_of_guid_concept_with_value_as_string
     {
         const   string  guid_value_as_string = "4AB92720-3138-4A7B-A7E9-2A49F6624736";
