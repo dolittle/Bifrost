e3e306ebc69006fa7ba13abbc1e210bf68c9f8bb
Einar Ingebrigtsen <einar@dolittle.com>
2013-12-02 15:47:43 +0100

    Introducing CommandType

--- Source/Bifrost.JavaScript/interaction/CommandOperation.js
+++ Source/Bifrost.JavaScript/interaction/CommandOperation.js
@@ -3,11 +3,15 @@
         /// <summary>Represents an operation that result in a command</summary>
         var self = this;
 
+        /// <field name="commandType" type="Bifrost.Type">Type of command to create</field>
+        this.commandType = null;
+
         this.createCommandOfType = function (commandType) {
             /// <summary>Create an instance of a given command type</summary>
-            commandType.create({
+            var instance = commandType.create({
                 region: self.region
             });
+            return instance;
         };
     })
 });
