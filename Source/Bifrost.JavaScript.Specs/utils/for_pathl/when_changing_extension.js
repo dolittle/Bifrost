855a3be2a8612bc5483269b3a70d288ec4f7aeb4
Einar Ingebrigtsen <einar@dolittle.com>
2013-12-06 16:04:51 +0100

    Started changing the casing for path to Path

--- Source/Bifrost.JavaScript.Specs/utils/for_pathl/when_changing_extension.js
+++ Source/Bifrost.JavaScript.Specs/utils/for_pathl/when_changing_extension.js
@@ -1,6 +1,6 @@
 ﻿describe("when changing extension", function () {
 
-    var newFile = Bifrost.path.changeExtension("Something/cool/file.html", "js");
+    var newFile = Bifrost.Path.changeExtension("Something/cool/file.html", "js");
 
     it("should change the extension", function () {
         expect(newFile).toBe("Something/cool/file.js");
