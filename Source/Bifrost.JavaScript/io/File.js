3932613be4d859fb2f56a1f75a87a4182e89462c
Einar Ingebrigtsen <einar@dolittle.com>
2013-12-09 14:45:34 +0100

    Putting in some Xml comments!

--- Source/Bifrost.JavaScript/io/File.js
+++ Source/Bifrost.JavaScript/io/File.js
@@ -1,6 +1,11 @@
 ﻿Bifrost.namespace("Bifrost.io", {
     File: Bifrost.Type.extend(function (path) {
+        /// <summary>Represents a file</summary>
+
+        /// <field name="type" type="Bifrost.io.fileType">Type of file represented</field>
         this.type = Bifrost.io.fileType.unknown;
+
+        /// <field name="path" type="Bifrost.Path">The path representing the file</field>
         this.path = Bifrost.Path.create({ fullPath: path });
     })
 });
--- Source/Bifrost.JavaScript/io/fileFactory.js
+++ Source/Bifrost.JavaScript/io/fileFactory.js
@@ -1,6 +1,12 @@
 ﻿Bifrost.namespace("Bifrost.io", {
     fileFactory: Bifrost.Singleton(function () {
+        /// <summary>Represents a factory for creating instances of Bifrost.io.File</summary>
         this.create = function (path, fileType) {
+            /// <summary>Creates a new file</summary>
+            /// <param name="path" type="String">Path of file</param>
+            /// <param name="fileType" type="Bifrost.io.fileType" optional="true">Type of file to use</param>
+            /// <returns type="Bifrost.io.File">An instance of a file</returns>
+
             var file = Bifrost.io.File.create({ path: path });
             if (!Bifrost.isNullOrUndefined(fileType)) {
                 file.fileType = fileType;
