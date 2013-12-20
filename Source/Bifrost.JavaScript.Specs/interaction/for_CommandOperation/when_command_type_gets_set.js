8cc13cfea6156f4bda0d5df99174f18fd99ad26c
Einar Ingebrigtsen <einar@dolittle.com>
2013-12-20 12:24:41 +0100

    Enabling command operations based on commands security

---
+++ Source/Bifrost.JavaScript.Specs/interaction/for_CommandOperation/when_command_type_gets_set.js
@@ -0,0 +1,31 @@
+﻿describe("when command type gets set", function () {
+    var commandType = { some: "commandType" };
+
+    var securityContext = {
+        isAuthorized: ko.observable("very much so")
+    };
+
+    var commandSecurityService = {
+        getContextForType: sinon.stub().returns(securityContext)
+    };
+
+    var region = {};
+
+    var context = {};
+
+    var instance = Bifrost.interaction.CommandOperation.create({
+        commandSecurityService: commandSecurityService,
+        region: region,
+        context: context
+    });
+
+    instance.commandType(commandType);
+
+    it("should get the security context for the type", function () {
+        expect(commandSecurityService.getContextForType.calledWith(commandType)).toBe(true);
+    });
+
+    it("should set can perform to what is in the security context", function () {
+        expect(instance.canPerform()).toBe(securityContext.isAuthorized());
+    });
+});
--- Source/Bifrost.JavaScript/interaction/CommandOperation.js
+++ Source/Bifrost.JavaScript/interaction/CommandOperation.js
@@ -1,10 +1,17 @@
 ﻿Bifrost.namespace("Bifrost.interaction", {
-    CommandOperation: Bifrost.interaction.Operation.extend(function () {
+    CommandOperation: Bifrost.interaction.Operation.extend(function (commandSecurityService) {
         /// <summary>Represents an operation that result in a command</summary>
         var self = this;
 
         /// <field name="commandType" type="Bifrost.Type">Type of command to create</field>
-        this.commandType = null;
+        this.commandType = ko.observable();
+
+        this.commandType.subscribe(function (type) {
+            var context = commandSecurityService.getContextForType(type);
+            self.canPerform(context.isAuthorized())
+
+        });
+
 
         this.createCommandOfType = function (commandType) {
             /// <summary>Create an instance of a given command type</summary>
