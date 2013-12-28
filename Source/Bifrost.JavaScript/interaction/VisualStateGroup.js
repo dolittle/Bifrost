5e6cf6ccef026432011c0dd8be8c44d6a02c7924
Einar Ingebrigtsen <einar@dolittle.com>
2013-12-28 16:56:13 +0100

    Disabling the posibility to add two states with the same name + adding XML comments!

--- Source/Bifrost.JavaScript/interaction/VisualStateGroup.js
+++ Source/Bifrost.JavaScript/interaction/VisualStateGroup.js
@@ -1,22 +1,38 @@
 Bifrost.namespace("Bifrost.interaction", {
 	VisualStateGroup: Bifrost.Type.extend(function() {
+		/// <summary>Represents a group that holds visual states</summary>
 		var self = this;
 
 		this.defaultDuration = Bifrost.TimeSpan.zero();
 
+		/// <field name="currentState" type="Bifrost.interaction.VisualState">Holds the current state, this is an observable</field>
 		this.currentState = ko.observable();
+
+		/// <field name="states" type="Array" elementType="Bifrost.interaction.VisualState">Holds an observable array of visual states</field>
 		this.states = ko.observableArray();
+
+		/// <field name="transitions" type="Array" elementType="Bifrost.interaction.VisualStateTransition">Holds an observable array of visual state transitions</field>
 		this.transitions = ko.observableArray();
 
 		this.addState = function(state) {
+			/// <summary>Add a state to the group</summary>
+			/// <parameter name="state" type="Bifrost.interaction.VisualState">State to add</parameter>
+			if( self.hasState(state.name) ) {
+				throw "VisualState with name of '"+state.name+"' already exists";
+			}
 			self.states.push(state);
 		};
 
 		this.addTransition = function(transition) {
+			/// <summary>Add transition to group</summary>
+			/// <parameter name="transition" type="Bifrost.interaction.VisualStateTransition">Transition to add</parameter>
 			self.transitions.push(transition);
 		};
 
 		this.hasState = function(stateName) {
+			/// <summary>Check if group has state by the name of the state</summary>
+			/// <parameter name="stateName" type="String">Name of the state to check for</parameter>
+			/// <returns type="Boolean">True if the state exists, false if not</returns>
 			var hasState = false;
 			self.states().forEach(function(state) {
 				if( state.name === stateName ) {
@@ -29,6 +45,9 @@ Bifrost.namespace("Bifrost.interaction", {
 		};
 
 		this.getStateByName = function(stateName) {
+			/// <summary>Gets a state by its name</summary>
+			/// <parameter name="stateName" type="String">Name of the state to get</parameter>
+			/// <returns type="Bifrost.interaction.VisualState">State found or null if it does not have a state by the given name</returns>
 			var stateFound = null;
 			self.states().forEach(function(state) {
 				if( state.name === stateName ) {
@@ -40,6 +59,8 @@ Bifrost.namespace("Bifrost.interaction", {
 		};
 
 		this.goTo = function(stateName) {
+			/// <summary>Go to a specific state by the name of the state</summary>
+			/// <parameter name="stateName" type="String">Name of the state to go to</parameter>
 			var state = self.getStateByName(stateName);
 			if( !Bifrost.isNullOrUndefined(state) ) {
 				self.currentState(state);
