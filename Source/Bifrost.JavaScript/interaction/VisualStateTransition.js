9e3fb0b5bb35b3f03a516b73d32d82721a8dc07d
Einar Ingebrigtsen <einar@dolittle.com>
2013-12-28 18:55:24 +0100

    XML Comments

--- Source/Bifrost.JavaScript/interaction/VisualStateTransition.js
+++ Source/Bifrost.JavaScript/interaction/VisualStateTransition.js
@@ -1,9 +1,15 @@
 Bifrost.namespace("Bifrost.interaction", {
 	VisualStateTransition: Bifrost.Type.extend(function() {
+		/// <summary>Represents a description of transition between two named states</summary>
 		var self = this;
 
+		/// <field name="from" type="String">Name of visual state that we are describing transitioning from</field>
 		this.from = "";
+
+		/// <field name="to" type="String">Name of visual state that we are describing transitioning to</field>
 		this.to = "";
+
+		/// <field name="duration" type="Bifrost.TimeStamp">Duration for the transition</field>
 		this.duration = Bifrost.TimeStamp.zero();
 	})
 });
