12b9d7e0baf53005355b2c0b601c0fd224f643d8
Einar Ingebrigtsen <einar@dolittle.com>
2014-01-03 15:21:21 +0100

    Modified the specs slightly + adding a spec for making sure that nested observables works!

--- Source/Bifrost.JavaScript.Specs/commands/for_CommandDescriptor/when_creating_a_list_of_parameters.js
+++ Source/Bifrost.JavaScript.Specs/commands/for_CommandDescriptor/when_creating_a_list_of_parameters.js
@@ -2,17 +2,22 @@
     var command = {
         name: "DoSomething",
         id: Bifrost.Guid.create(),
-        plainString: "test",
-        koString: ko.observable("test")
+        plainString: "plainString",
+        koString: ko.observable("test"),
+        observableWithObservableAsValue: ko.observable(ko.observable(42))
     };
     var commandDescriptor = Bifrost.commands.CommandDescriptor.createFrom(command);
-
+    var result = JSON.parse(commandDescriptor.command);
 
     it("should include the plainString", function () {
-        expect(JSON.parse(commandDescriptor.command).plainString).toBeDefined();
+        expect(result.plainString).toBe(command.plainString)
     });
 
     it("should include the koString", function () {
-        expect(JSON.parse(commandDescriptor.command).koString).toBeDefined();
+        expect(result.koString).toBe(command.koString());
+    });
+
+    it("should include the observable with observable value", function () {
+        expect(result.observableWithObservableAsValue).toBe(command.observableWithObservableAsValue()());
     });
 });
