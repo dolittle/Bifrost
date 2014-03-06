310c8b3d290393f8c8ee910836d79f62be67a346
Einar Ingebrigtsen <einar@dolittle.com>
2014-03-06 21:29:22 +0100

    If the object coming in is null or undefined; then it is not a type.. :)

--- Source/Bifrost.JavaScript/utils/isType.js
+++ Source/Bifrost.JavaScript/utils/isType.js
@@ -1,5 +1,6 @@
 Bifrost.namespace("Bifrost", {
-	isType : function(o) {
+    isType: function (o) {
+        if (Bifrost.isNullOrUndefined(o)) return false;
 		return typeof o._typeId != "undefined";
 	}
 });
