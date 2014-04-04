2a7879a54d899d762f997cbf80442672be12b0b4
smithmx <smithmx@gmail.com>
2014-04-04 10:18:43 +0200

    Add an interceptor for dealing with UTC Date Times

---
+++ Source/Bifrost.NHibernate/Read/UTCDateTimesInterceptor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Type;
+
+namespace Bifrost.NHibernate.Read
+{
+    public class UTCDateTimesInterceptor : EmptyInterceptor
+    {
+        public override bool OnLoad(object entity, object id, object[] state,string[] propertyNames, IType[] types)
+        {
+            ConvertDatabaseDateTimeToUtc(state, types);
+            return true;
+        }
+
+        private void ConvertDatabaseDateTimeToUtc(object[] state, IList<IType> types)
+        {
+            for (int i = 0; i < types.Count; i++)
+            {
+                if (!IsDateTime(types[i].ReturnedClass))
+                    continue;
+
+                var dateTime = state[i] as DateTime?;
+
+                if (!dateTime.HasValue)
+                    continue;
+
+                if (dateTime.Value.Kind != DateTimeKind.Unspecified)
+                    continue;
+
+                state[i] = DateTime.SpecifyKind(dateTime.Value, DateTimeKind.Utc);
+            }
+        }
+
+        bool IsDateTime(Type type)
+        {
+            return type == typeof (DateTime) || type == typeof (DateTime?);
+        }
+    }
+}
