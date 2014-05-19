ce6989d506fad8c72c32617672a9ca8992fac12a
Einar Ingebrigtsen <einar@dolittle.com>
2014-05-19 07:59:24 +0200

    Adding CanPerform and CanUndo

--- Source/Bifrost.Client/Interaction/Operation.cs
+++ Source/Bifrost.Client/Interaction/Operation.cs
@@ -34,12 +34,33 @@ namespace Bifrost.Interaction
         }
 
         /// <summary>
+        /// Check if the operation can be performed
+        /// </summary>
+        /// <param name="context"><see cref="OperationContext"/> in which the operation is performed in</param>
+        /// <returns>True if it can be performed, false if not</returns>
+        public virtual bool CanPerform(OperationContext context)
+        {
+            return false;
+        }
+
+
+        /// <summary>
         /// Undo the given operation based on the state coming out of the perform
         /// </summary>
         /// <param name="context"><see cref="OperationContext"/> in which the operation was performed in</param>
         /// <param name="state">State as a result from when it was performed</param>
         public virtual void Undo(OperationContext context, OperationState state)
         {
+        }
+
+        /// <summary>
+        /// Check if the operation can be undoed
+        /// </summary>
+        /// <param name="context"><see cref="OperationContext"/> in which the operation was performed in</param>
+        /// <returns>True if it can be undoed, false if not</returns>
+        public virtual bool CanUndo(OperationContext context)
+        {
+            return false;
         }
     }
 }
