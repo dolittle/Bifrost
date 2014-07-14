1b2908cf54a1e1ae28e6d11a755830251a07a02d
Pavneet Singh Saund <pav.saund@gmail.com>
2014-07-14 19:58:58 +0200

    Added check to create test results folder if needed

--- Build/RunJavascriptSpecs.cmd
+++ Build/RunJavascriptSpecs.cmd
@@ -1,1 +1,5 @@
-..\Tools\Forseti\Forseti.TRX.exe "..\Source\Forseti.yaml" ..\TestResults\forseti.testresults.trx APPVEYOR
+@echo off
+call CreateTestResultsDir.cmd
+@echo on
+..\Tools\Forseti\Forseti.TRX.exe "..\Source\Forseti.yaml" "..\TestResults\forseti.testresults.trx" "BUILD-CI"
+@echo off
