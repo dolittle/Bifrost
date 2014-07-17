065b197cf3f1a724091f3e9459f98a575018be1c
Einar Ingebrigtsen <einar@dolittle.com>
2014-07-17 14:01:20 +0200

    Making sure the type conversion works for observable targets as well!

---
+++ Source/Bifrost.JavaScript.Specs/mapping/for_mapper/when_mapping_to_instance_with_observable_property_with_mismatching_type.js
@@ -0,0 +1,29 @@
+describe("when mapping to instance with observable property with mismatching types", function(){
+	
+	var data = { numberProperty: "42"};
+
+	var parameters = {
+	    typeConverters: {
+	        convertFrom: sinon.stub().returns(42)
+	    }
+	};
+
+	var type = Bifrost.Type.extend(function () {
+        this.numberProperty = ko.observable(0);
+    });
+
+	var mappedInstance = type.create();
+
+	(function becauseOf(){
+	    var mapper = Bifrost.mapping.mapper.create(parameters);
+		mapper.mapToInstance(type, data, mappedInstance);
+	})();
+
+	it("should type convert the value", function () {
+	    expect(parameters.typeConverters.convertFrom.calledWith(data.numberProperty, "Number")).toBe(true);
+	});
+
+	it("should map the corresponding numberProprty value", function(){
+		expect(mappedInstance.numberProperty()).toEqual(42);
+	});
+});
