ad85ee0a8bb63bcf7bdb781ddf3228d1b4159920
Einar Ingebrigtsen <einar@dolittle.com>
2014-10-27 15:43:11 +0100

    QueryValidation descriptors up and running - now for the specs!

--- Source/Bifrost/Read/Validation/QueryValidationDescriptors.cs
+++ Source/Bifrost/Read/Validation/QueryValidationDescriptors.cs
@@ -16,33 +16,45 @@
 // limitations under the License.
 //
 #endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Bifrost.Execution;
+using Bifrost.Extensions;
+
 namespace Bifrost.Read.Validation
 {
     /// <summary>
     /// Represents an implementation of <see cref="IQueryValidationDescriptors"/> 
     /// </summary>
+    [Singleton]
     public class QueryValidationDescriptors : IQueryValidationDescriptors
     {
+        Dictionary<Type, IQueryValidationDescriptor> _descriptors = new Dictionary<Type, IQueryValidationDescriptor>();
+
         /// <summary>
         /// Initializes an instance of <see cref="QueryValidationDescriptors"/>
         /// </summary>
         public QueryValidationDescriptors(ITypeDiscoverer typeDiscoverer, IContainer container)
         {
-            var descriptors = typeDiscoverer.FindMultiple(typeof(QueryValidationDescriptorFor<>));
+            var descriptors = typeDiscoverer.FindMultiple(typeof(QueryValidationDescriptorFor<>)).Where(d => d != typeof(QueryValidationDescriptorFor<>));
+            descriptors.ForEach(d => {
+                var queryType = d.GetGenericArguments()[0];
+                var descriptor = container.Get(d) as IQueryValidationDescriptor;
+                _descriptors[queryType] = descriptor;
+            });
         }
 
 
 #pragma warning disable 1591 // Xml Comments
         public bool HasDescriptorFor<TQuery>() where TQuery : IQuery
         {
-            return false; 
-            //throw new System.NotImplementedException();
+            return _descriptors.ContainsKey(typeof(TQuery)); 
         }
 
         public IQueryValidationDescriptor GetDescriptorFor<TQuery>() where TQuery : IQuery
         {
-            throw new System.NotImplementedException();
+            return _descriptors[typeof(TQuery)];
         }
 #pragma warning restore 1591 // Xml Comments
     }
