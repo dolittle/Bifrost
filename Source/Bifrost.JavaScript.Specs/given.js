ac955daf190fcb2a96479a442df3096edb79720d
Einar Ingebrigtsen <einar@dolittle.com>
2015-01-05 21:39:48 +0100

    Added given for specs

---
+++ Source/Bifrost.JavaScript.Specs/given.js
@@ -0,0 +1,26 @@
+﻿var contextsByName = {};
+
+function given(name, context) {
+    /*
+    var file = "given/" + name.replaceAll(" ", "_") + ".js";
+    print("File : " + file);
+    require([file], function () {
+        print("Yes we can");
+    });*/
+
+    
+    if (contextsByName.hasOwnProperty(name)) {
+        function weaved() {
+            contextsByName[name].prototype = this;
+            context.prototype = new contextsByName[name]();
+            return new context();
+        }
+
+        return weaved;
+
+    } else {
+        contextsByName[name] = context;
+    }
+
+    return context;    
+}
