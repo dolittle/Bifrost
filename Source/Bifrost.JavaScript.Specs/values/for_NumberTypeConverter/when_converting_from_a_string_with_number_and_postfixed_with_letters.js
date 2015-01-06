c80ca05ebb6aa1c8425ce5b452a8c2ef0f5d2833
Einar Ingebrigtsen <einar@dolittle.com>
2015-01-06 15:58:20 +0100

    Extending the number conversion. Fixes #583

---
+++ Source/Bifrost.JavaScript.Specs/values/for_NumberTypeConverter/when_converting_from_a_string_with_number_and_postfixed_with_letters.js
@@ -0,0 +1,14 @@
+﻿describe("when converting from a string with number and postfixed with letters", function () {
+
+    var converter = Bifrost.values.NumberTypeConverter.create();
+
+    var result = converter.convertFrom("42something");
+
+    it("should be a number", function () {
+        expect(result.constructor).toBe(Number);
+    });
+
+    it("should convert to correct number", function () {
+        expect(result).toBe(42);
+    });
+});
--- Source/Bifrost.JavaScript/values/NumberTypeConverter.js
+++ Source/Bifrost.JavaScript/values/NumberTypeConverter.js
@@ -1,8 +1,35 @@
 ﻿Bifrost.namespace("Bifrost.values", {
     NumberTypeConverter: Bifrost.values.TypeConverter.extend(function () {
+        var allowedCharacters = "0123456789.,";
+
         this.supportedType = Number;
 
+        function stripLetters(value) {
+            value = value.toString();
+            var returnValue = "";
+
+            for (var charIndex = 0; charIndex < value.length; charIndex++) {
+                var found = false;
+                for (var allowedCharIndex = 0; allowedCharIndex < allowedCharacters.length; allowedCharIndex++) {
+                    if (value[charIndex] == allowedCharacters[allowedCharIndex]) {
+                        found = true;
+                        returnValue = returnValue + value[charIndex];
+                        break;
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+
         this.convertFrom = function (value) {
+            if (value.constructor === Number) {
+                return value;
+            }
+            if (value === "") {
+                return 0;
+            }
+            value = stripLetters(value);
             var result = 0;
             if (value.indexOf(".") >= 0) {
                 result = parseFloat(value);
