ed66fa9264adbebc8a7c43bd592ef787c4381671
Einar Ingebrigtsen <einar@dolittle.com>
2015-01-22 16:09:34 +0100

    If we ask undefined if its an object, it should return false

--- Source/Bifrost.JavaScript/utils/isObject.js
+++ Source/Bifrost.JavaScript/utils/isObject.js
@@ -1,6 +1,6 @@
 Bifrost.namespace("Bifrost", {
     isObject: function (o) {
-        if (o === null) {
+        if (o === null || typeof o == "undefined" ) {
             return false;
         }
         return Object.prototype.toString.call(o) === '[object Object]';
