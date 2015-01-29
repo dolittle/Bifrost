86ff27daab73bb5be2f4825133d367a1c0454555
Einar Ingebrigtsen <einar@dolittle.com>
2015-01-29 07:56:25 +0100

    Enabling XML Documentations and treat all warnings as errors

--- Source/Bifrost.CommonServiceLocator/ConfigurationExtensions.cs
+++ Source/Bifrost.CommonServiceLocator/ConfigurationExtensions.cs
@@ -17,14 +17,23 @@
 //
 #endregion
 using Bifrost.CommonServiceLocator;
+using Bifrost.Execution;
 using Microsoft.Practices.ServiceLocation;
 
 namespace Bifrost.Configuration
 {
+    /// <summary>
+    /// Provides extensions for configuration
+    /// </summary>
     public static class ConfigurationExtensions
     {
         static bool _configured = false;
 
+        /// <summary>
+        /// Enables common service locator and sets the <see cref="IContainer">container</see> as the source of location
+        /// </summary>
+        /// <param name="configure"><see cref="IConfigure"/> object to configure</param>
+        /// <returns>Chained <see cref="IConfigure"/></returns>
         public static IConfigure UsingCommonServiceLocator(this IConfigure configure)
         {
             if (!_configured)
