da87054107da420f3bf960721ad3b748fb2a31a0
Einar Ingebrigtsen <einar@dolittle.com>
2015-02-25 16:40:55 +0100

    Starting a QuickStart sample for WPF

---
+++ Source/Bifrost.QuickStart.WPF/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Windows;
+using Bifrost.Configuration;
+using Bifrost.Configuration.AssemblyLocator;
+
+namespace Bifrost.QuickStart.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        static App()
+        {
+            Configure.DiscoverAndConfigure(a => a.IncludeAll().ExceptAssembliesStartingWith("Bifrost"));
+        }
+    }
+}
