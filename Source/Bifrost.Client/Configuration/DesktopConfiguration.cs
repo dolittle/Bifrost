2df56727dc9e4a7737ecf0aec3940713ffbc9b93
Einar Ingebrigtsen <einar@dolittle.com>
2015-03-02 08:22:15 +0100

    Putting in place DesktopConfiguration

---
+++ Source/Bifrost.Client/Configuration/DesktopConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Bifrost.Commands;
+using Bifrost.Execution;
+using Bifrost.Extensions;
+
+namespace Bifrost.Configuration
+{
+    public class DesktopConfiguration : IFrontendTargetConfiguration
+    {
+        public void Initialize(IContainer container)
+        {
+            container.Bind(typeof(ICommandFor<>), (t) =>
+            {
+                var commandForProxies = container.Get<ICommandForProxies>();
+                return commandForProxies.CallGenericMethod<object, ICommandForProxies>(cc => cc.GetFor<Bifrost.Commands.ICommand>, t.GenericTypeArguments[0]);
+            });
+            
+        }
+    }
+}
