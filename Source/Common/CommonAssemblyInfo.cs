ae0aec684e2b15f1fffb230828e31615d6508958
Einar Ingebrigtsen <einar@dolittle.com>
2015-03-02 12:18:28 +0100

    Release of 1.0.0.13

--- Source/Common/CommonAssemblyInfo.cs
+++ Source/Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2008-2015 Dolittle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.12")]
-[assembly: AssemblyFileVersion("1.0.0.12")]
+[assembly: AssemblyVersion("1.0.0.13")]
+[assembly: AssemblyFileVersion("1.0.0.13")]
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
