ad202631b96b277cd6bd47ddb635529e019dafeb
Einar Ingebrigtsen <einar@dolittle.com>
2015-03-04 16:20:41 +0100

    Version 1.0.0.15 going out

--- Source/Common/CommonAssemblyInfo.cs
+++ Source/Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2008-2015 Dolittle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.14")]
-[assembly: AssemblyFileVersion("1.0.0.14")]
+[assembly: AssemblyVersion("1.0.0.15")]
+[assembly: AssemblyFileVersion("1.0.0.15")]
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
