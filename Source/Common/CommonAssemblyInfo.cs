a6866167b1676a031d29b41721278dfbd3b66e25
Einar Ingebrigtsen <einar@dolittle.com>
2015-06-09 10:19:48 +0200

    Version 1.0.0.23 out - nuspecs updated to .NET 4.5.1

--- Source/Common/CommonAssemblyInfo.cs
+++ Source/Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2008-2015 Dolittle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.22")]
-[assembly: AssemblyFileVersion("1.0.0.22")]
+[assembly: AssemblyVersion("1.0.0.23")]
+[assembly: AssemblyFileVersion("1.0.0.23")]
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
