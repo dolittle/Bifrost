02f73cf58e2641f0229a5d200b95b6cfa09ac22e
Børge Nordli <bnordli@gmail.com>
2015-06-12 13:09:52 +0200

    Check for observable parameters when computing areAllParametersSet in Query.js

---
+++ Source/Bifrost.JavaScript.Specs/read/for_Query/when_asking_if_all_parameters_are_set_and_one_observable_parameter_is_not_set.js
@@ -0,0 +1,22 @@
+﻿describe("when asking if all parameters are set and one observable parameter is not set", function () {
+
+    var queryType = Bifrost.read.Query.extend(function () {
+        var self = this;
+
+        this.firstParameter = ko.observable(1);
+        this.secondParameter = ko.observable(ko.observable());
+    });
+
+    var queryableFactory = {};
+    var region = {};
+
+    var instance = queryType.create({
+        queryableFactory: queryableFactory,
+        region: region
+    });
+    var result = instance.areAllParametersSet();
+
+    it("should return false", function () {
+        expect(result).toBe(false);
+    });
+});
