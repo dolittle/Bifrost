1401719b2ec662a604f9c6e83d1e6d23fc3a38ba
Einar Ingebrigtsen <einar@dolittle.com>
2015-06-22 11:43:45 +0200

    Existing specs for TypeDiscoverer mended

--- Source/Bifrost.Specs/Execution/for_TypeDiscoverer/given/a_type_discoverer.cs
+++ Source/Bifrost.Specs/Execution/for_TypeDiscoverer/given/a_type_discoverer.cs
@@ -1,41 +1,44 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Reflection;
-//using System.Runtime.InteropServices;
-//using Bifrost.Execution;
-//using Machine.Specifications;
-//using Moq;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Bifrost.Execution;
+using Machine.Specifications;
+using Moq;
 
-//namespace Bifrost.Specs.Execution.for_TypeDiscoverer.given
-//{
-//    public class a_type_discoverer
-//    {
-//        protected static TypeDiscoverer type_discoverer;
-//        protected static Mock<_Assembly> assembly_mock;
-//        protected static Type[] types;
+namespace Bifrost.Specs.Execution.for_TypeDiscoverer.given
+{
+    public class a_type_discoverer
+    {
+        protected static TypeDiscoverer type_discoverer;
+        protected static Mock<_Assembly> assembly_mock;
+        protected static Type[] types;
 
-//        protected static Mock<IAssemblies> assemblies_mock;
-//        protected static Mock<ITypeFinder> type_finder_mock;
+        protected static Mock<IAssemblies> assemblies_mock;
+        protected static Mock<ITypeFinder> type_finder_mock;
+        protected static Mock<IContractToImplementorsMap> contract_to_implementors_map_mock;
 
-//        Establish context = () =>
-//                                {
-//                                    types = new[] {
-//                                        typeof(ISingle),
-//                                        typeof(Single),
-//                                        typeof(IMultiple),
-//                                        typeof(FirstMultiple),
-//                                        typeof(SecondMultiple)
-//                                    };
+        Establish context = () =>
+                                {
+                                    types = new[] {
+                                        typeof(ISingle),
+                                        typeof(Single),
+                                        typeof(IMultiple),
+                                        typeof(FirstMultiple),
+                                        typeof(SecondMultiple)
+                                    };
 
-//                                    assembly_mock = new Mock<_Assembly>();
-//                                    assembly_mock.Setup(a => a.GetTypes()).Returns(types);
-//                                    assembly_mock.Setup(a => a.FullName).Returns("A.Full.Name");
+                                    assembly_mock = new Mock<_Assembly>();
+                                    assembly_mock.Setup(a => a.GetTypes()).Returns(types);
+                                    assembly_mock.Setup(a => a.FullName).Returns("A.Full.Name");
 
-//                                    assemblies_mock = new Mock<IAssemblies>();
-//                                    assemblies_mock.Setup(x => x.GetAll()).Returns(new[] { assembly_mock.Object });
+                                    assemblies_mock = new Mock<IAssemblies>();
+                                    assemblies_mock.Setup(x => x.GetAll()).Returns(new[] { assembly_mock.Object });
 
-//                                    type_finder_mock = new Mock<ITypeFinder>();
-//                                    type_discoverer = new TypeDiscoverer(assemblies_mock.Object, type_finder_mock.Object);
-//                                };
-//    }
-//}
+                                    contract_to_implementors_map_mock = new Mock<IContractToImplementorsMap>();
+
+                                    type_finder_mock = new Mock<ITypeFinder>();
+                                    type_discoverer = new TypeDiscoverer(assemblies_mock.Object, type_finder_mock.Object, contract_to_implementors_map_mock.Object);
+                                };
+    }
+}
