7ee3ae7670cb4334341dc42774db2fa10d04d4f6
Børge Nordli <bnordli@gmail.com>
2015-12-01 10:29:07 +0100

    Add test helpers to mock IInstancesOf

---
+++ Source/Bifrost.Testing/TestExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Bifrost.Testing.Fakes.Configuration;
+
+namespace Bifrost.Testing
+{
+    public static class TestExtensions
+    {
+        public static TestInstancesOf<T> NoInstancesOf<T>() where T : class
+        {
+            return new TestInstancesOf<T>();
+        }
+
+        public static TestInstancesOf<T> SingleInstanceOf<T>(this T instance) where T : class
+        {
+            return new TestInstancesOf<T>(instance);
+        }
+
+        public static TestInstancesOf<T> AsInstancesOf<T>(this IEnumerable<T> instances) where T : class
+        {
+            return new TestInstancesOf<T>(instances);
+        }
+    }
+}
