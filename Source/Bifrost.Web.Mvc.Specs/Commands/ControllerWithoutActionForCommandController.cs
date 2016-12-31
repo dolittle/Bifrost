27a81e08e49df6736bf52dace7da27d0e04307e7
Einar Ingebrigtsen <einar@dolittle.com>
2016-12-31 16:20:33 +0100

    Removing ASP.NET MVC support

--- Source/Bifrost.Web.Mvc.Specs/Commands/ControllerWithoutActionForCommandController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web.Mvc;
-using Bifrost.Testing.Fakes.Commands;
-
-namespace Bifrost.Web.Mvc.Specs.Commands
-{
-    public class ControllerWithoutActionForCommandController : Controller
-    {
-    }
-}
--- Source/Bifrost.Web.Mvc/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Bifrost.Web.Mvc")]
-[assembly: AssemblyDescription("Bifrost support for ASP.net MVC specifics")]
