ec73c947493e0c668637975ae2de61bb82b0f3ce
Einar Ingebrigtsen <einar@dolittle.com>
2017-01-01 11:29:24 +0100

    Forgot to save renamed file and project

--- Source/Bifrost.Ninject.Specs/for_KernelExtensions/when_loading_all_modules_and_there_are_two_modules_in_the_assembly.cs
+++ Source/Bifrost.Ninject.Specs/for_KernelExtensions/when_loading_all_modules_and_there_are_two_modules_in_the_assembly.cs
@@ -5,7 +5,7 @@ using Ninject.Modules;
 
 namespace Bifrost.Ninject.Specs.for_KernelExtensions
 {
-    public class when_loading_all_modules_and_there_are_two_modules_in_one_assembly : given.a_kernel_and_a_type_importer
+    public class when_loading_all_modules_and_there_are_two_modules_in_the_assembly : given.a_kernel_and_a_type_importer
     {
         static IEnumerable<INinjectModule>    result;
 
