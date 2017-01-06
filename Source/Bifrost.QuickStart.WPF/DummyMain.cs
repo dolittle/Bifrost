755d8ab4e67f71cbd080cf192dd2558f4097b7c2
Einar Ingebrigtsen <einar@dolittle.com>
2017-01-06 13:27:56 +0100

    Conditionally compiling only on Windows_NT

---
+++ Source/Bifrost.QuickStart.WPF/DummyMain.cs
@@ -0,0 +1,13 @@
+namespace Bifrost.QuickStart.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public class DummyMain
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
