65a182637dc11263cbe0f5ebdd2336ed2b5afa8d
Einar Ingebrigtsen <einar@dolittle.com>
2017-01-06 13:27:12 +0100

    Calling the Event with the right info at the right time

--- Source/Bifrost/Execution/FileSystemAssemblyProvider.cs
+++ Source/Bifrost/Execution/FileSystemAssemblyProvider.cs
@@ -31,8 +31,6 @@ namespace Bifrost.Execution
             assemblyFiles.AddRange(fileSystem.GetFilesFrom(assemblyFileInfo.Directory.ToString(), "*.exe"));
 
             AvailableAssemblies = assemblyFiles.Select(file => new AssemblyInfo(Path.GetFileNameWithoutExtension(file.FullName), file.FullName));
-            
-            foreach( var assembly in AvailableAssemblies ) AssemblyAdded(assembly);
         }
 
 #pragma warning disable 1591 // Xml Comments
@@ -42,7 +40,9 @@ namespace Bifrost.Execution
 
         public Assembly Get(AssemblyInfo assemblyInfo)
         {
-            return Assembly.Load(new AssemblyName(assemblyInfo.Name));
+            var assembly = Assembly.Load(new AssemblyName(assemblyInfo.Name));
+            AssemblyAdded(assembly);
+            return assembly;
         }
 #pragma warning restore 1591 // Xml Comments
     }
