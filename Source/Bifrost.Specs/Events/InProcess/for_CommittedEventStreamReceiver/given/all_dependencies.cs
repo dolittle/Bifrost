da2532bf5ef35f1e1a9149c0db536ecd7e9c29a5
Einar Ingebrigtsen <einar@dolittle.com>
2017-01-15 00:31:41 +0100

    Specs in place for the receiver

---
+++ Source/Bifrost.Specs/Events/InProcess/for_CommittedEventStreamReceiver/given/all_dependencies.cs
@@ -0,0 +1,13 @@
+﻿using Bifrost.Events.InProcess;
+using Machine.Specifications;
+using Moq;
+
+namespace Bifrost.Specs.Events.InProcess.for_CommittedEventStreamReceiver.given
+{
+    public class all_dependencies
+    {
+        protected static Mock<ICommittedEventStreamBridge> committed_event_stream_bridge;
+
+        Establish context = () => committed_event_stream_bridge = new Mock<ICommittedEventStreamBridge>();
+    }
+}
---
+++ Source/Bifrost.Specs/Events/InProcess/for_CommittedEventStreamReceiver/when_committed_event_stream_is_received.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Bifrost.Events;
+using Machine.Specifications;
+
+namespace Bifrost.Specs.Events.InProcess.for_CommittedEventStreamReceiver
+{
+    public class when_committed_event_stream_is_received : given.a_committed_event_stream_receiver
+    {
+        static CommittedEventStream committed_event_stream;
+        static CommittedEventStream received_committed_event_stream;
+
+        Establish context = () =>
+        {
+            committed_event_stream = new CommittedEventStream(Guid.NewGuid());
+            committed_event_stream_receiver.Received += c => received_committed_event_stream = c;
+        };
+
+        Because of = () => committed_event_stream_bridge.Raise(c => c.Received += null, committed_event_stream);
+
+        It should_forward_the_received_event_stream = () => received_committed_event_stream.ShouldEqual(committed_event_stream);
+    }
+}
