01d6a64352485d6e095bf6973d16a2ede3410e11
B. Nordli <bnordli@gmail.com>
2017-01-25 09:29:48 +0100

    Include AssemblySpecifier for Web projects

---
+++ Source/Bifrost.Default/AssemblySpecifier.cs
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using Bifrost.Configuration.Assemblies;
+using Bifrost.Execution;
+
+namespace Web
+{
+    public class AssemblySpecifier : ICanSpecifyAssemblies
+    {
+        public void Specify(IAssembliesConfiguration configuration)
+        {
+            configuration.IncludeAssembliesStartingWith("Web.dll");
+        }
+    }
+}
