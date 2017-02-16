2e2a12304816fc7ff05f6d72a1b735a7b451cf80
Einar Ingebrigtsen <einar@dolittle.com>
2017-02-16 13:41:25 +0100

    Unused using statement

--- Source/Bifrost/Strings/StringFormatParser.cs
+++ Source/Bifrost/Strings/StringFormatParser.cs
@@ -2,7 +2,6 @@
  *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-using System;
 using System.Text.RegularExpressions;
 
 namespace Bifrost.Strings
