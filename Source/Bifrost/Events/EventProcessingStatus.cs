215b83138f31d5b5cc381035c293734da91b73a1
Einar Ingebrigtsen <einar@dolittle.com>
2017-02-21 19:34:20 +0100

    Adding the different statuses an event processor can have

--- Source/Bifrost/Events/EventProcessingStatus.cs
+++ Source/Bifrost/Events/EventProcessingStatus.cs
@@ -12,6 +12,21 @@ namespace Bifrost.Events
         /// <summary>
         /// The state an <see cref="IEventProcessor"/> is in when it has processed successfully
         /// </summary>
-        Successful
+        Successful,
+
+        /// <summary>
+        /// The state an <see cref="IEventProcessor"/> is in when it is been given an event in the wrong sequence
+        /// </summary>
+        WrongSequence,
+
+        /// <summary>
+        /// The state an <see cref="IEventProcessor"/> is in when it has been given an event multiple times
+        /// </summary>
+        Duplicate,
+
+        /// <summary>
+        /// The state an <see cref="IEventProcessor"/> is in when it has problems processing
+        /// </summary>
+        Failed
     }
 }
