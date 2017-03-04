4006f09b07704ff2925d6fc7530d11afc19a315e
Einar Ingebrigtsen <einar@dolittle.com>
2017-03-04 19:08:33 +0100

    Changed behavior - we don't want the "on" method called
    When applying an event for the first time to an event source, we don't want it to call the on method. There is no value in doing that as things are now.

--- Source/Bifrost.Specs/Events/for_EventSource/when_applying_a_new_event.cs
+++ Source/Bifrost.Specs/Events/for_EventSource/when_applying_a_new_event.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Bifrost.Events;
+﻿using System.Linq;
 using Bifrost.Testing.Fakes.Events;
 using Machine.Specifications;
 
 namespace Bifrost.Specs.Events.for_EventSource
 {
-	[Subject(Subjects.applying_events)]
+    [Subject(Subjects.applying_events)]
 	public class when_applying_a_new_event : given.a_stateful_event_source
 	{
 		Establish context =
@@ -20,6 +18,6 @@ namespace Bifrost.Specs.Events.for_EventSource
 		It should_add_the_event_to_the_uncommited_events = () => event_source.UncommittedEvents.Select(e=>e.Event).ShouldContainOnly(@event);
 		It should_increment_the_sequence_of_the_version = () => event_source.Version.Sequence.ShouldEqual(1);
 		It should_not_increment_the_commit_of_the_version = () => event_source.Version.Commit.ShouldEqual(0);
-	    It should_have_applied_the_event = () => event_source.EventApplied.ShouldBeTrue();
+	    It should_not_call_the_on_method_for_the_event = () => event_source.EventApplied.ShouldBeFalse();
 	}
 }
