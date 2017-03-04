b963e0b66b091dd5ed2b9846b8aa1a476950ee66
Einar Ingebrigtsen <einar@dolittle.com>
2017-03-04 16:18:59 +0100

    Removing EventStore implementation - create if needed later

--- Source/Bifrost.RavenDB/ConfigurationExtensions.cs
+++ Source/Bifrost.RavenDB/ConfigurationExtensions.cs
@@ -5,45 +5,11 @@
 using System;
 using System.Net;
 using Bifrost.RavenDB;
-using Bifrost.RavenDB.Events;
 
 namespace Bifrost.Configuration
 {
     public static class ConfigurationExtensions
     {
-        public static IConfigure UsingRavenDB(this IEventsConfiguration eventsConfiguration, Action<RegularEventStoreConfiguration> configureCallback)
-        {
-            eventsConfiguration.EventStore = typeof(EventStore);
-            eventsConfiguration.EventSubscriptions = typeof(EventSubscriptions);
-            
-            var configuration = new RegularEventStoreConfiguration();
-            configureCallback(configuration);
-            Configure.Instance.Container.Bind<IEventStoreConfiguration>(configuration);
-
-            var eventSubscriptionsConfiguration = new RegularEventSubscriptionsConfiguration();
-            configuration.CopyTo(eventSubscriptionsConfiguration);
-            Configure.Instance.Container.Bind<IEventSubscriptionsConfiguration>(eventSubscriptionsConfiguration);
-
-            return Configure.Instance;
-        }
-
-        public static RegularEventStoreConfiguration WithUrl(this RegularEventStoreConfiguration configuration, string url)
-        {
-            configuration.Url = url;
-            return configuration;
-        }
-
-        public static RegularEventStoreConfiguration WithCredentials(this RegularEventStoreConfiguration configuration, ICredentials credentials)
-        {
-            configuration.Credentials = credentials;
-            return configuration;
-        }
-
-        public static RegularEventStoreConfiguration WithDefaultDatabase(this RegularEventStoreConfiguration configuration, string defaultDatabase)
-        {
-            configuration.DefaultDatabase = defaultDatabase;
-            return configuration;
-        }
 
         public static IConfigure UsingRavenDB(this IHaveStorage storage, Action<EntityContextConfiguration> configureCallback = null)
         {
