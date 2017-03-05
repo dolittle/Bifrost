aea146893fb3b0cb9f5d7126216ab182ac85007a
Einar Ingebrigtsen <einar@dolittle.com>
2017-03-05 17:07:37 +0100

    Fixing specs

--- Source/Bifrost.Specs/Commands/for_CommandContext/given/a_command_context_for_a_simple_command_with_one_tracked_object_with_one_uncommitted_event.cs
+++ Source/Bifrost.Specs/Commands/for_CommandContext/given/a_command_context_for_a_simple_command_with_one_tracked_object_with_one_uncommitted_event.cs
@@ -1,15 +1,21 @@
-﻿using Bifrost.Testing.Fakes.Events;
+﻿using Bifrost.Events;
+using Bifrost.Testing.Fakes.Events;
 using Machine.Specifications;
+using Moq;
 
 namespace Bifrost.Specs.Commands.for_CommandContext.given
 {
     public class a_command_context_for_a_simple_command_with_one_tracked_object_with_one_uncommitted_event : a_command_context_for_a_simple_command_with_one_tracked_object
     {
         protected static SimpleEvent    uncommitted_event;
+        protected static Mock<IEventEnvelope> event_envelope;
 
         Establish context = () =>
         {
             uncommitted_event = new SimpleEvent(aggregated_root.EventSourceId);
+            event_envelope = new Mock<IEventEnvelope>();
+            event_envelope.SetupGet(e => e.EventSourceId).Returns(aggregated_root.EventSourceId);
+            event_envelopes.Setup(e => e.CreateFrom(aggregated_root, uncommitted_event)).Returns(event_envelope.Object);
             aggregated_root.Apply(uncommitted_event);
         };
     }
