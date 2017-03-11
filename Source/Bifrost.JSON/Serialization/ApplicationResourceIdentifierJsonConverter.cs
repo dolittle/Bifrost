da6dbe689afe922d284bf0f988040880856c30b5
Einar Ingebrigtsen <einar@dolittle.com>
2017-03-11 16:37:18 +0100

    Adding support for reading back application resource identifiers

--- Source/Bifrost.JSON/Serialization/ApplicationResourceIdentifierJsonConverter.cs
+++ Source/Bifrost.JSON/Serialization/ApplicationResourceIdentifierJsonConverter.cs
@@ -27,36 +27,9 @@ namespace Bifrost.JSON.Serialization
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            return null;
-
-            /*
-            try
-            {
-                var jsonObject = JObject.Load(reader);
-                var className = jsonObject.Property("ClassName").Value.ToString();
-                var name = jsonObject.Property("Name").Value.ToString();
-                var assemblyName = jsonObject.Property("AssemblyName").Value.ToString();
-                var signature = jsonObject.Property("Signature").Value.ToString();
-
-                var start = signature.IndexOf('(') + 1;
-                var end = signature.IndexOf(')');
-                var parametersString = signature.Substring(start, end - start);
-                var parameterTypeStrings = parametersString.Split(',');
-
-                if (string.IsNullOrEmpty(className) ||
-                    string.IsNullOrEmpty(name) ||
-                    string.IsNullOrEmpty(assemblyName) ||
-                    parameterTypeStrings == null)
-                    throw new ArgumentException("Json object is not representing a MethodInfo");
-
-                var typeName = string.Format("{0}, {1}", className, assemblyName);
-                var type = Type.GetType(typeName);
-                var method = type.GetTypeInfo().GetMethods().Where(m => m.Name.Equals(name) && DoesSignatureMatch(m, parameterTypeStrings)).SingleOrDefault();
-                return method;
-            }
-            catch {
-                return null;
-            }*/
+            var identifierAsString = reader.ReadAsString();
+            var identifier = _converter.FromString(identifierAsString);
+            return identifier;
         }
 
         public override bool CanWrite { get { return true; } }
