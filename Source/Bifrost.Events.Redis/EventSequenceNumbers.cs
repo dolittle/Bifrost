71ae2dcfadc9f1ba471d24dc30b5ff3b1efeedcb
Einar Ingebrigtsen <einar@dolittle.com>
2017-03-29 02:05:58 +0200

    Redis support for EventSequencing

--- Source/Bifrost.Events.Redis/EventSequenceNumbers.cs
+++ Source/Bifrost.Events.Redis/EventSequenceNumbers.cs
@@ -2,6 +2,8 @@
  *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using System.Security.Claims;
+using System.Security.Principal;
 using Bifrost.Applications;
 using StackExchange.Redis;
 
