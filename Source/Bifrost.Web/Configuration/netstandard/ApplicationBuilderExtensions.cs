05097c8e60d2f7638f153cb2775d04b9cffb07e8
Einar Ingebrigtsen <einar@dolittle.com>
2017-03-31 23:27:53 +0200

    Started on the configuration extensions for ASP.NET Core

--- Source/Bifrost.Web/Configuration/netstandard/ApplicationBuilderExtensions.cs
+++ Source/Bifrost.Web/Configuration/netstandard/ApplicationBuilderExtensions.cs
@@ -1,19 +1,31 @@
-/*---------------------------------------------------------------------------------------------
+﻿/*---------------------------------------------------------------------------------------------
  *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using Microsoft.AspNetCore.Builder;
+using Bifrost.Web.Proxies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Bifrost.Configuration;
 
 namespace Bifrost.Web.Configuration
 {
     public static class ApplicationBuilderExtensions
     {
+        public static void AddBifrost(this IServiceCollection serviceCollection)
+        {
+            Configure.DiscoverAndConfigure();
+
+        }
 
         public static IApplicationBuilder UseBifrost(this IApplicationBuilder builder)
         {
-            app.Map("/Bifrost/Application", a => a.Run(async (context) => await context.Response.WriteAsync("Hello from App")));
-
+            builder.Map("/Bifrost/Application", a => a.Run(async (context) =>
+            {
+                var proxies = Configure.Instance.Container.Get<GeneratedProxies>();
+                await context.Response.WriteAsync("Hello from App");
+            }));
             return builder;
         }
-    }
+}
 }
