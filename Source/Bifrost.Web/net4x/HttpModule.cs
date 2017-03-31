4dee4bf9376951f0a4a8afdcb1be787e204ec8dd
Einar Ingebrigtsen <einar@dolittle.com>
2017-03-31 23:27:31 +0200

    Removing unused usings

--- Source/Bifrost.Web/net4x/HttpModule.cs
+++ Source/Bifrost.Web/net4x/HttpModule.cs
@@ -1,11 +1,10 @@
-/*---------------------------------------------------------------------------------------------
+﻿/*---------------------------------------------------------------------------------------------
  *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using System;
 using System.Collections.Generic;
 using System.Web;
-using Bifrost.Web.Pipeline;
 
 namespace Bifrost.Web
 {
