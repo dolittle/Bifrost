b00121b72575127757a600498b050db230fab2c6
Einar Ingebrigtsen <einar@dolittle.com>
2017-04-02 21:23:14 +0200

    Started implementation of Rabbit support

---
+++ Source/Bifrost.Events.RabbitMQ/CommittedEventStreamSender.cs
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Bifrost.Events.RabbitMQ
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="ICanSendCommittedEventStream"/>
+    /// </summary>
+    public class CommittedEventStreamSender : ICanSendCommittedEventStream
+    {
+        /// <inheritdoc/>
+        public void Send(CommittedEventStream committedEventStream)
+        {
+
+        }
+    }
+}
---
+++ Source/Bifrost.Events.RabbitMQ/ConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Bifrost.Events
+{
+    /// <summary>
+    /// Extensions for configuring RabbitMQ related communication for <see cref="IEvent">events</see>
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static CommittedEventStreamSenderConfiguration UsingRabbitMQ(this CommittedEventStreamSenderConfiguration configuration)
+        {
+            configuration.CommittedEventStreamSender = typeof(RabbitMQ.CommittedEventStreamSender);
+
+            return configuration;
+        }
+    }
+}
