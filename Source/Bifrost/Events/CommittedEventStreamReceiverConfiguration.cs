806dcd5457a76b7d8f42645de704b34ba93d3774
Einar Ingebrigtsen <einar@dolittle.com>
2017-04-02 21:21:59 +0200

    Moving configuration of sending/receiving into own objects

---
+++ Source/Bifrost/Events/CommittedEventStreamReceiverConfiguration.cs
@@ -0,0 +1,28 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Bifrost.Events.InProcess;
+
+namespace Bifrost.Events
+{
+    /// <summary>
+    /// Represents the configuration related to <see cref="ICanReceiveCommittedEventStream"/>
+    /// </summary>
+    public class CommittedEventStreamReceiverConfiguration
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="CommittedEventStreamReceiverConfiguration"/>
+        /// </summary>
+        public CommittedEventStreamReceiverConfiguration()
+        {
+            CommittedEventStreamReceiver = typeof(CommittedEventStreamReceiver);
+        }
+
+        /// <summary>
+        /// Gets or sets the type of <see cref="ICanReceiveCommittedEventStream"/> to use for handling sending of <see cref="CommittedEventStream"/>
+        /// </summary>
+        public Type CommittedEventStreamReceiver { get; set; }
+    }
+}
---
+++ Source/Bifrost/Events/CommittedEventStreamSenderConfiguration.cs
@@ -0,0 +1,28 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Bifrost.Events.InProcess;
+
+namespace Bifrost.Events
+{
+    /// <summary>
+    /// Represents the configuration related to <see cref="ICanSendCommittedEventStream"/>
+    /// </summary>
+    public class CommittedEventStreamSenderConfiguration
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="CommittedEventStreamSenderConfiguration"/>
+        /// </summary>
+        public CommittedEventStreamSenderConfiguration()
+        {
+            CommittedEventStreamSender = typeof(CommittedEventStreamSender);
+        }
+
+        /// <summary>
+        /// Gets or sets the type of <see cref="ICanSendCommittedEventStream"/> to use for handling sending of <see cref="CommittedEventStream"/>
+        /// </summary>
+        public Type CommittedEventStreamSender { get; set; }
+    }
+}
