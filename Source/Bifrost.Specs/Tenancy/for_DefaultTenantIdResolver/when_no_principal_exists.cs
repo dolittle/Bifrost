bbd9d3c1b0bd9d3e190155f443862598e3056cf7
Einar Ingebrigtsen <einar@dolittle.com>
2017-04-11 13:34:00 +0200

    Context should return null for ClaimsPrincipalSelector

--- Source/Bifrost.Specs/Tenancy/for_DefaultTenantIdResolver/when_no_principal_exists.cs
+++ Source/Bifrost.Specs/Tenancy/for_DefaultTenantIdResolver/when_no_principal_exists.cs
@@ -1,4 +1,5 @@
-﻿using Bifrost.Tenancy;
+﻿using System.Security.Claims;
+using Bifrost.Tenancy;
 using Machine.Specifications;
 
 namespace Bifrost.Specs.Tenancy.for_DefaultTenantIdResolver
@@ -6,6 +7,8 @@ namespace Bifrost.Specs.Tenancy.for_DefaultTenantIdResolver
     public class when_no_principal_exists : given.a_default_tenant_id_resolver
     {
         static TenantId result;
+
+        Establish context = () => ClaimsPrincipal.ClaimsPrincipalSelector = () => null;
 
         Because of = () => result = resolver.Resolve();
 
