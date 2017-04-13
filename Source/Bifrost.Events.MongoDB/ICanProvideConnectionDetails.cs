e05bdf6c9881d51149128b4fb78d5930881589f9
Einar Ingebrigtsen <einar@dolittle.com>
2017-04-13 12:47:18 +0200

    Putting in configuration for connectionstring and database
    Related to #807

---
+++ Source/Bifrost.Events.MongoDB/ICanProvideConnectionDetails.cs
@@ -0,0 +1,14 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+
+namespace Bifrost.Events.MongoDB
+{
+    /// <summary>
+    /// Delegate for providing connection string for <see cref="EventStore"/>
+    /// </summary>
+    /// <returns></returns>
+    public delegate Tuple<string,string> ICanProvideConnectionDetails();
+}
