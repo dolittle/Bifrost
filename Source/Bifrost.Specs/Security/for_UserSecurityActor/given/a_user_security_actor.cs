86cdb4fbb37b6386735cd7c6c802ed4b09529b40
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-05 18:52:42 +0200

    Supporting ClaimType with Value rules for securables
    Fixes #851

---
+++ Source/Bifrost.Specs/Security/for_UserSecurityActor/given/a_user_security_actor.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using Bifrost.Security;
+using Machine.Specifications;
+using Moq;
+
+namespace Bifrost.Specs.Security.for_UserSecurityActor.given
+{
+    public class a_user_security_actor
+    {
+        protected static UserSecurityActor actor;
+        protected static ClaimsIdentity identity;
+        protected static ClaimsPrincipal principal;
+
+        Establish context = () =>
+        {
+            identity = new ClaimsIdentity();
+            principal = new ClaimsPrincipal(identity);
+
+            var principalResolver = new Mock<ICanResolvePrincipal>();
+            principalResolver.Setup(p => p.Resolve()).Returns(principal);
+
+            actor = new UserSecurityActor(principalResolver.Object);
+        };
+    }
+}
