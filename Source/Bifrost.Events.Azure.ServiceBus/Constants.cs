0ea746f1aa649daf600bbdfe6a3a72201bf42e89
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-09 09:09:44 +0200

    Introducing Azure ServiceBus support - very naive

---
+++ Source/Bifrost.Events.Azure.ServiceBus/Constants.cs
@@ -0,0 +1,17 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Bifrost.Events.Azure.ServiceBus
+{
+    /// <summary>
+    /// Holds constants used
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// The name of the queue being used
+        /// </summary>
+        public const string QueueName = "BifrostQueue";
+    }
+}
